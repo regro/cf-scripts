80bacbabd2be61176aba353ef5d37c7161bccc1e
christopher <cjwright4242@gmail.com>
2018-03-04 22:48:27 -0500

    print current number of out of date packages at end of graph_upstream

--- 02-graph_upstream.py
+++ 02-graph_upstream.py
@@ -91,6 +91,9 @@ for node, attrs in gx.node.items():
             print(datetime.datetime.utcfromtimestamp(ts)
                   .strftime('%Y-%m-%dT%H:%M:%SZ'))
         pass
+print('Current number of out of date packages: '
+      '{}'.format(len([n for n, a in gx.node.items()
+                       if a['new_version'] != a['version']])))
 print('writing out file')
 del parse_version
 # nx.write_yaml(gx, 'graph.yml')
