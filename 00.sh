51c8f7525b255834dea1c719f831c90ae73dd8d7
christopher <cjwright4242@gmail.com>
2018-03-13 13:38:27 -0400

    echo pwd for testing

--- 00.sh
+++ 00.sh
@@ -4,4 +4,5 @@
 export PATH=~/mc/bin:$PATH
 conda-forge-tick --run 0
 conda-forge-tick --run 1
-doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
+echo "$(pwd)"
+#doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
