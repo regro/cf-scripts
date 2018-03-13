ebb8d67f501c70a3abda0eec1615ea54be8ac1af
christopher <cjwright4242@gmail.com>
2018-03-13 14:52:46 -0400

    ENH: add upstream tracking to 00

--- 00.sh
+++ 00.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+export START_TIME="$(date +%s)"
+export TIMEOUT=2700
 ./setup.sh
 cd ../cf-graph
 export PATH=~/mc/bin:$PATH
 conda-forge-tick --run 0
 conda-forge-tick --run 1
+conda-forge-tick --run 2
+#xonsh ../cf-scripts/03-auto_tick.xsh
 doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
