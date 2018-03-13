abf6a73c3528837f40d5dda1c32f8d010e56f07b
christopher <cjwright4242@gmail.com>
2018-03-13 13:22:04 -0400

    export path in all scripts

--- 00.sh
+++ 00.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ./setup.sh
+export PATH=~/mc/bin:$PATH
 conda-forge-tick --run 0
 conda-forge-tick --run 1
 doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
--- 03.sh
+++ 03.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ./setup.sh
+export PATH=~/mc/bin:$PATH
 conda-forge-tick --run 2
 xonsh ../cf-scripts/03-auto_tick.xsh
 doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
