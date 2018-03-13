2c6e86bfe1febc63ede121fdc56bf549cc33474c
christopher <cjwright4242@gmail.com>
2018-03-13 12:28:47 -0400

    clone in travis

--- setup.sh
+++ setup.sh
@@ -12,7 +12,6 @@ conda update conda --yes
 
 export GIT_FULL_HASH=`git rev-parse HEAD`
 conda install python=$TRAVIS_PYTHON_VERSION
-git clone https://github.com/regro/cf-scripts.git
 git clone https://github.com/regro/cf-graph.git
 cd cf-graph
 conda install --file ../cf-scripts/requirements/run
