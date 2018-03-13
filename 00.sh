282c13479f09f85d071a8fba6483e33b745e1dd6
christopher <cjwright4242@gmail.com>
2018-03-13 18:01:19 -0400

    WIP: add PR script to 00

--- 00.sh
+++ 00.sh
@@ -8,5 +8,6 @@ export PATH=~/mc/bin:$PATH
 conda-forge-tick --run 0
 conda-forge-tick --run 1
 conda-forge-tick --run 2
-#xonsh ../cf-scripts/03-auto_tick.xsh
+doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
+xonsh ../cf-scripts/03-auto_tick.xsh
 doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
