ceca3b82f88128fbf901d6fab81c154a4f4e2940
christopher <cjwright4242@gmail.com>
2018-03-14 17:23:01 -0400

    00-03 are all copies of 00

--- 01.sh
+++ 01.sh
@@ -5,8 +5,9 @@ export TIMEOUT=2700
 ./setup.sh
 cd ../cf-graph
 export PATH=~/mc/bin:$PATH
-#conda-forge-tick --run 1
-#conda-forge-tick --run 2
-#doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
+conda-forge-tick --run 0
+conda-forge-tick --run 1
+conda-forge-tick --run 2
+doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
 xonsh ../cf-scripts/03-auto_tick.xsh
 doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
--- 02.sh
+++ 02.sh
@@ -5,6 +5,7 @@ export TIMEOUT=2700
 ./setup.sh
 cd ../cf-graph
 export PATH=~/mc/bin:$PATH
+conda-forge-tick --run 0
 conda-forge-tick --run 1
 conda-forge-tick --run 2
 doctr deploy --token --built-docs . --deploy-repo regro/cf-graph --deploy-branch-name master .
