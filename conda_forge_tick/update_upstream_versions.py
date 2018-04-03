11bf3795a327143a6047f0884aa8b4d9ee83da86
christopher <cjwright4242@gmail.com>
2018-04-03 16:21:23 -0400

    don't replace ``_`` with ``-`` in cran. Attn: @isuruf

--- conda_forge_tick/update_upstream_versions.py
+++ conda_forge_tick/update_upstream_versions.py
@@ -107,7 +107,7 @@ class CRAN(LibrariesIO):
 
     def get_version(self, url):
         ver = LibrariesIO.get_version(self, url)
-        return str(ver).replace('-', '_')
+        return str(ver)
 
 
 class RawURL:
