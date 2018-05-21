ec31f8572d30f347f7ae660289c7142e27acfd91
Justin Calamari <justin.calamari@gmail.com>
2018-05-20 21:17:04 -0400

    Put sub_graph generation in its own function

--- conda_forge_tick/make_graph.py
+++ conda_forge_tick/make_graph.py
@@ -10,6 +10,68 @@ import logging
 logger = logging.getLogger("conda_forge_tick.make_graph")
 
 
+def get_attrs(name, i, bad):
+    sub_graph = {
+        'time': time.time(),
+        'feedstock_name': name,
+    }
+
+    logger.info((i, name))
+    r = requests.get('https://raw.githubusercontent.com/'
+                     'conda-forge/{}-feedstock/master/recipe/'
+                     'meta.yaml'.format(name))
+    if r.status_code != 200:
+        logger.warn('Something odd happened when fetching recipe '
+                    '{}: {}'.format(name, r.status_code))
+        bad.append(name)
+        sub_graph['bad'] = r.status_code
+        return sub_graph
+
+    text = r.content.decode('utf-8')
+    yaml_dict = parsed_meta_yaml(text)
+    if not yaml_dict:
+        logger.warn('Something odd happened when parsing recipe '
+                    '{}'.format(name))
+        bad.append(name)
+        sub_graph['bad'] = 'Could not parse'
+        return sub_graph
+    # TODO: Write schema for dict
+    req = yaml_dict.get('requirements', set())
+    if req:
+        build = list(req.get('build', []) if req.get(
+            'build', []) is not None else [])
+        run = list(req.get('run', []) if req.get(
+            'run', []) is not None else [])
+        req = build + run
+        req = set([x.split()[0] for x in req])
+
+    keys = [('source', 'url'), ('package', 'name'),
+            ('package', 'version')]
+    missing_keys = [k[1] for k in keys if k[1] not in
+                    yaml_dict.get(k[0], {})]
+    if missing_keys:
+        logger.warn("Recipe {} doesn't have a {}".format(name,
+                    ', '.join(missing_keys)))
+        bad.append(name)
+        sub_graph['bad'] = ', '.join(missing_keys)
+    sub_graph.update({
+        'name': yaml_dict.get('package').get('name'),
+        'version': str(yaml_dict.get('package').get('version')),
+        'url': yaml_dict.get('source').get('url'),
+        'req': req,
+        'time': time.time(),
+        'feedstock_name': name,
+        'meta_yaml': yaml_dict,
+        'raw_meta_yaml': text,
+    })
+    k = next(iter((set(yaml_dict['source'].keys()) &
+                   hashlib.algorithms_available)), None)
+    if k:
+        sub_graph['hash_type'] = k
+
+    return sub_graph
+
+
 def make_graph(names, gx=None):
 
     bad = []
@@ -28,78 +90,14 @@ def make_graph(names, gx=None):
 
     for i, name in enumerate(total_names):
         try:
-            sub_graph = {
-                'time': time.time(),
-                'feedstock_name': name,
-            }
-
-            logger.info((i, name))
-            r = requests.get('https://raw.githubusercontent.com/'
-                             'conda-forge/{}-feedstock/master/recipe/'
-                             'meta.yaml'.format(name))
-            if r.status_code != 200:
-                logger.warn('Something odd happened when fetching recipe '
-                            '{}: {}'.format(name, r.status_code))
-                bad.append(name)
-                sub_graph['bad'] = r.status_code
-                if name in new_names:
-                    gx.add_node(name, **sub_graph)
-                else:
-                    gx.nodes[name].update(**sub_graph)
-                continue
-
-            text = r.content.decode('utf-8')
-            yaml_dict = parsed_meta_yaml(text)
-            if not yaml_dict:
-                logger.warn('Something odd happened when parsing recipe '
-                            '{}'.format(name))
-                bad.append(name)
-                sub_graph['bad'] = 'Could not parse'
-                if name in new_names:
-                    gx.add_node(name, **sub_graph)
-                else:
-                    gx.nodes[name].update(**sub_graph)
-                continue
-            # TODO: Write schema for dict
-            req = yaml_dict.get('requirements', set())
-            if req:
-                build = list(req.get('build', []) if req.get(
-                    'build', []) is not None else [])
-                run = list(req.get('run', []) if req.get(
-                    'run', []) is not None else [])
-                req = build + run
-                req = set([x.split()[0] for x in req])
-
-            keys = [('source', 'url'), ('package', 'name'),
-                    ('package', 'version')]
-            missing_keys = [k[1] for k in keys if k[1] not in
-                            yaml_dict.get(k[0], {})]
-            if missing_keys:
-                logger.warn("Recipe {} doesn't have a {}".format(name,
-                            ', '.join(missing_keys)))
-                bad.append(name)
-                sub_graph['bad'] = ', '.join(missing_keys)
-            sub_graph.update({
-                'name': yaml_dict.get('package').get('name'),
-                'version': str(yaml_dict.get('package').get('version')),
-                'url': yaml_dict.get('source').get('url'),
-                'req': req,
-                'time': time.time(),
-                'feedstock_name': name,
-                'meta_yaml': yaml_dict,
-                'raw_meta_yaml': text,
-            })
-            k = next(iter((set(yaml_dict['source'].keys()) &
-                           hashlib.algorithms_available)), None)
-            if k:
-                sub_graph['hash_type'] = k
-
+            sub_graph = get_attrs(name, i, bad)
+        except Exception as e:
+            logger.warn('Error adding {} to the graph: {}'.format(name, e))
+        else:
             if name in new_names:
                 gx.add_node(name, **sub_graph)
             else:
                 gx.nodes[name].update(**sub_graph)
-        except Exception as e:
-            logger.warn('Error adding {} to the graph: {}'.format(name, e))
 
     for node, attrs in gx.node.items():
         for dep in attrs['req']:
