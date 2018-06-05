54cf33947b04ee8b614c75b5c4b042d3994da90a
Justin Calamari <justin.calamari@gmail.com>
2018-06-05 15:28:17 -0400

    STY: PEP8

--- conda_forge_tick/tests/test_path_lengths.py
+++ conda_forge_tick/tests/test_path_lengths.py
@@ -1,5 +1,6 @@
 from conda_forge_tick.path_lengths import get_levels
 
+
 def test_get_levels():
     levels = {0: {'a'}, 1: {'d'}, 2: {'f', 'g', 'h'}}
     assert get_levels('test_graph1.pkl', 'a') == levels
@@ -8,8 +9,7 @@ def test_get_levels():
     print(get_levels('test_graph2.pkl', 'a'))
     assert get_levels('test_graph2.pkl', 'a') == levels
 
-    levels = {0: {'a'}, 1: {'b'}, 2: {'d'}, 3: {'c', 'f'}, 4: {'e', 'h'}, 5: {'g'}}
+    levels = {0: {'a'}, 1: {'b'}, 2: {'d'}, 3: {'c', 'f'},
+              4: {'e', 'h'}, 5: {'g'}}
     print(get_levels('test_graph3.pkl', 'a'))
     assert get_levels('test_graph3.pkl', 'a') == levels
-
-test_get_levels()
