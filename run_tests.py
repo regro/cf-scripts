eea2cbed59ce41ca2b7c65b93613f12e78de8e0e
christopher <cjwright4242@gmail.com>
2018-06-09 23:00:21 -0400

    add tests to args

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ import pytest
 
 if __name__ == '__main__':
     # show output results from every test function
-    args = ['-v']
+    args = ['-v', 'tests']
     # show the message output for skipped and expected failure tests
     if len(sys.argv) > 1:
         args.extend(sys.argv[1:])
