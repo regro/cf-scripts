e2da3edbbfe2c35635f273834840426d9b7d487b
christopher <cjwright4242@gmail.com>
2018-06-21 13:49:49 -0400

    use converted named tuples for PRed

--- migrate_to_hash.py
+++ migrate_to_hash.py
@@ -1,15 +1,16 @@
 import networkx as nx
 
 from conda_forge_tick.migrators import Compiler
+from conda_forge_tick.utils import convert_dict_to_nt
 
 g = nx.read_gpickle('../cf-graph/graph.pkl')
 
 # Migrate the PRed versions
 for node, attrs in g.nodes:
     if 'PRed' in attrs:
-        attrs['PRed'] = {'class': 'Version',
-                         'class_version': 0,
-                         'version': attrs['PRed']}
+        attrs['PRed'] = {convert_dict_to_nt({'class_name': 'Version',
+                                             'class_version': 0,
+                                             'version': attrs['PRed']})}
 
 # Don't migrate already done Compilers (double commits cause problems)
 m = Compiler()
@@ -22,7 +23,7 @@ for node, attrs in g.nodes:
 last_compiler_pr = 'cxxopts'
 last_compiler_index = compiler_migrations.index(last_compiler_pr)
 for i in range(last_compiler_index):
-    g.nodes[compiler_migrations[i]]['PRed'] = {
-        'class': 'Compiler', 'class_version': 0}
+    g.nodes[compiler_migrations[i]]['PRed'] = {convert_dict_to_nt({
+        'class_name': 'Compiler', 'class_version': 0})}
 
 nx.write_gpickle(g, '../cf-graph/graph.pkl')
