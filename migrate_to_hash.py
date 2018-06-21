a444dc56543a9eb6681b3ce8b15d2d64f482b599
christopher <cjwright4242@gmail.com>
2018-06-21 15:42:37 -0400

    change migrator uids for script

--- migrate_to_hash.py
+++ migrate_to_hash.py
@@ -9,8 +9,8 @@ g = nx.read_gpickle('../cf-graph/graph.pkl')
 # Migrate the PRed versions
 for node, attrs in g.nodes:
     if 'PRed' in attrs:
-        attrs['PRed'] = {convert_dict_to_nt({'class_name': 'Version',
-                                             'class_version': 0,
+        attrs['PRed'] = {convert_dict_to_nt({'migrator_name': 'Version',
+                                             'migrator_version': 0,
                                              'version': attrs['PRed']})}
 
 # Don't migrate already done Compilers (double commits cause problems)
@@ -25,6 +25,6 @@ last_compiler_pr = 'cxxopts'
 last_compiler_index = compiler_migrations.index(last_compiler_pr)
 for i in range(last_compiler_index):
     g.nodes[compiler_migrations[i]]['PRed'] = {convert_dict_to_nt({
-        'class_name': 'Compiler', 'class_version': 0})}
+        'migrator_name': 'Compiler', 'migrator_version': 0})}
 
 nx.write_gpickle(g, '../cf-graph/graph.pkl')
