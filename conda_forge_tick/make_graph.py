e85d1303991cec4b7496a48d79a60fa4505a06e4
christopher <cjwright4242@gmail.com>
2018-07-02 22:34:41 -0400

    also use futures to get data from github

--- conda_forge_tick/make_graph.py
+++ conda_forge_tick/make_graph.py
@@ -1,4 +1,6 @@
 import hashlib
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import collections.abc
 import time
 import networkx as nx
@@ -12,68 +14,72 @@ logger = logging.getLogger("conda_forge_tick.make_graph")
 
 
 def get_attrs(name, i):
-    sub_graph = {
-        "time": time.time(),
-        "feedstock_name": name,
-        # All feedstocks start out as good
-        "bad": False,
-    }
+    try:
+        sub_graph = {
+            "time": time.time(),
+            "feedstock_name": name,
+            # All feedstocks start out as good
+            "bad": False,
+        }
 
-    logger.info((i, name))
-    r = requests.get(
-        "https://raw.githubusercontent.com/"
-        "conda-forge/{}-feedstock/master/recipe/"
-        "meta.yaml".format(name)
-    )
-    if r.status_code != 200:
-        logger.warn(
-            "Something odd happened when fetching recipe "
-            "{}: {}".format(name, r.status_code)
+        logger.info((i, name))
+        r = requests.get(
+            "https://raw.githubusercontent.com/"
+            "conda-forge/{}-feedstock/master/recipe/"
+            "meta.yaml".format(name)
         )
-        sub_graph["bad"] = "make_graph: {}".format(r.status_code)
-        return sub_graph
+        if r.status_code != 200:
+            logger.warn(
+                "Something odd happened when fetching recipe "
+                "{}: {}".format(name, r.status_code)
+            )
+            sub_graph["bad"] = "make_graph: {}".format(r.status_code)
+            return sub_graph
 
-    text = r.content.decode("utf-8")
-    sub_graph["raw_meta_yaml"] = text
-    yaml_dict = parse_meta_yaml(text)
-    if not yaml_dict:
-        logger.warn("Something odd happened when parsing recipe " "{}".format(name))
-        sub_graph["bad"] = "make_graph: Could not parse"
-        return sub_graph
-    sub_graph["meta_yaml"] = yaml_dict
-    # TODO: Write schema for dict
-    req = yaml_dict.get("requirements", set())
-    if req:
-        build = list(req.get("build", []) if req.get("build", []) is not None else [])
-        host = list(req.get("host", []) if req.get("host", []) is not None else [])
-        run = list(req.get("run", []) if req.get("run", []) is not None else [])
-        req = build + host + run
-        req = set([x.split()[0] for x in req])
-    sub_graph["req"] = req
+        text = r.content.decode("utf-8")
+        sub_graph["raw_meta_yaml"] = text
+        yaml_dict = parse_meta_yaml(text)
+        if not yaml_dict:
+            logger.warn("Something odd happened when parsing recipe " "{}".format(name))
+            sub_graph["bad"] = "make_graph: Could not parse"
+            return sub_graph
+        sub_graph["meta_yaml"] = yaml_dict
+        # TODO: Write schema for dict
+        req = yaml_dict.get("requirements", set())
+        if req:
+            build = list(req.get("build", []) if req.get("build", []) is not None else [])
+            host = list(req.get("host", []) if req.get("host", []) is not None else [])
+            run = list(req.get("run", []) if req.get("run", []) is not None else [])
+            req = build + host + run
+            req = set([x.split()[0] for x in req])
+        sub_graph["req"] = req
 
-    keys = [("package", "name"), ("package", "version")]
-    missing_keys = [k[1] for k in keys if k[1] not in yaml_dict.get(k[0], {})]
-    source = yaml_dict.get("source", [])
-    if isinstance(source, collections.abc.Mapping):
-        source = [source]
-    source_keys = set()
-    for s in source:
-        if not sub_graph.get("url"):
-            sub_graph["url"] = s.get("url")
-        source_keys |= s.keys()
-    if "url" not in source_keys:
-        missing_keys.append("url")
-    if missing_keys:
-        logger.warn("Recipe {} doesn't have a {}".format(name, ", ".join(missing_keys)))
-        sub_graph["bad"] = "make_graph: missing {}".format(", ".join(missing_keys))
-    for k in keys:
-        if k[1] not in missing_keys:
-            sub_graph[k[1]] = yaml_dict[k[0]][k[1]]
-    k = next(iter((source_keys & hashlib.algorithms_available)), None)
-    if k:
-        sub_graph["hash_type"] = k
+        keys = [("package", "name"), ("package", "version")]
+        missing_keys = [k[1] for k in keys if k[1] not in yaml_dict.get(k[0], {})]
+        source = yaml_dict.get("source", [])
+        if isinstance(source, collections.abc.Mapping):
+            source = [source]
+        source_keys = set()
+        for s in source:
+            if not sub_graph.get("url"):
+                sub_graph["url"] = s.get("url")
+            source_keys |= s.keys()
+        if "url" not in source_keys:
+            missing_keys.append("url")
+        if missing_keys:
+            logger.warn("Recipe {} doesn't have a {}".format(name, ", ".join(missing_keys)))
+            sub_graph["bad"] = "make_graph: missing {}".format(", ".join(missing_keys))
+        for k in keys:
+            if k[1] not in missing_keys:
+                sub_graph[k[1]] = yaml_dict[k[0]][k[1]]
+        k = next(iter((source_keys & hashlib.algorithms_available)), None)
+        if k:
+            sub_graph["hash_type"] = k
 
-    return sub_graph
+        return name, sub_graph
+    except Exception as e:
+        logger.warn("Error adding {} to the graph: {}".format(name, e))
+        return None, None
 
 
 def make_graph(names, gx=None):
@@ -89,16 +95,16 @@ def make_graph(names, gx=None):
     total_names = new_names + old_names
     logger.info("start loop")
 
-    for i, name in enumerate(total_names):
-        try:
-            sub_graph = get_attrs(name, i)
-        except Exception as e:
-            logger.warn("Error adding {} to the graph: {}".format(name, e))
-        else:
-            if name in new_names:
-                gx.add_node(name, **sub_graph)
-            else:
-                gx.nodes[name].update(**sub_graph)
+    with ThreadPoolExecutor(max_workers=20) as pool:
+        futures = [get_attrs(name, i) for i, name in enumerate(total_names)]
+
+        for f in as_completed(futures):
+            name, sub_graph = f.result()
+            if name and sub_graph:
+                if name in new_names:
+                    gx.add_node(name, **sub_graph)
+                else:
+                    gx.nodes[name].update(**sub_graph)
 
     for node, attrs in gx.node.items():
         for dep in attrs.get("req", []):
