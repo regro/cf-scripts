b4732742dc23299e845054296cf7dc3732a6c6d5
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-19 22:40:29 -0400

    make into a string

--- conda_forge_tick/status_report.py
+++ conda_forge_tick/status_report.py
@@ -12,7 +12,7 @@ def main(args=None):
 
     for migrator in migrators:
         if isinstance(migrator, Rebuild):
-            total_status[migrator] = f'{migrator.name} Migration Status'
+            total_status[migrator.name] = f'{migrator.name} Migration Status'
             migrator_name = migrator.__class__.__name__.lower()
             if migrator_name == 'rebuild':
                 migrator_name = migrator.name.lower().replace(" ", "")
