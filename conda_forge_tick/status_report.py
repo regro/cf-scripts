0a9e972ccb0611f36f7f288e5d0c7ffcbf0215ff
Christopher J. Wright <cjwright4242@gmail.com>
2019-09-11 11:32:51 -0400

    write out status for unmaintained feedstocks

--- conda_forge_tick/status_report.py
+++ conda_forge_tick/status_report.py
@@ -6,25 +6,36 @@ import json
 
 def main(args=None):
     gx, *_, migrators = initialize_migrators(do_rebuild=True)
-    if not os.path.exists('./status'):
-        os.mkdir('./status')
-    total_status = {} 
+    if not os.path.exists("./status"):
+        os.mkdir("./status")
+    total_status = {}
 
     for migrator in migrators:
         if isinstance(migrator, Rebuild):
             migrator_name = migrator.__class__.__name__.lower()
-            if migrator_name == 'rebuild':
+            if migrator_name == "rebuild":
                 migrator_name = migrator.name.lower().replace(" ", "")
-            total_status[migrator_name] = f'{migrator.name} Migration Status'
+            total_status[migrator_name] = f"{migrator.name} Migration Status"
             status, build_order = migrator_status(migrator, gx)
-            with open(os.path.join(f'./status/{migrator_name}.json'), 'w') as fo:
-                json.dump(
-                    status,
-                    fo,
-                    indent=2
-                )
-    with open('./status/total_status.json', 'w') as f:
+            with open(os.path.join(f"./status/{migrator_name}.json"), "w") as fo:
+                json.dump(status, fo, indent=2)
+    with open("./status/total_status.json", "w") as f:
         json.dump(total_status, f)
+    with open("./status/unmaintained.json", "w") as f:
+        l = [
+            k
+            for k, v in gx.nodes.items()
+            if len(
+                [
+                    z
+                    for z in v.get("payload", {}).get("PRed", [])
+                    if z.get("PR", {}).get("state", "closed") == "open"
+                ]
+            )
+            >= 3
+        ]
+        json.dump(l, f)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
