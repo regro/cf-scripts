966ee0d1ea605d88d4cad3a9833d8ea9767020df
Christopher J. Wright <cjwright4242@gmail.com>
2020-01-28 21:18:41 -0500

    fix more compact

--- conda_forge_tick/status_report.py
+++ conda_forge_tick/status_report.py
@@ -33,7 +33,7 @@ def main(args: Any = None) -> None:
             with tempfile.NamedTemporaryFile() as ntf, open(f"{ntf.name}.dot", "w") as f:
                 f.write(d.decode('utf-8'))
                 # make the graph a bit more compact
-                d = Source(subprocess.check_output(['unflatten', '-f', '-l', '5', '-c', '10', f"{ntf.name}.dot"])).pipe('svg')
+                d = Source(subprocess.check_output(['unflatten', '-f', '-l', '5', '-c', '10', f"{ntf.name}.dot"]).decode('utf-8')).pipe('svg')
             with open(os.path.join(f"./status/{migrator_name}.svg"), "wb") as fb:
                 fb.write(d)
 
