02d62b4c6164906fc47a88262bd2b97cabc59aa5
David Hoese <david.hoese@ssec.wisc.edu>
2020-02-28 10:50:31 -0600

    Add extra jinja2 variable removal migrator

--- conda_forge_tick/migrators/extra_jinj2a_keys_cleanup.py
+++ conda_forge_tick/migrators/extra_jinj2a_keys_cleanup.py
@@ -5,45 +5,63 @@ from ruamel.yaml import YAML
 
 from conda_forge_tick.xonsh_utils import indir
 from conda_forge_tick.migrators.core import MiniMigrator
+from conda_forge_tick.recipe_parser import CondaMetaYAML
 
 if typing.TYPE_CHECKING:
     from ..migrators_types import AttrsTypedDict
 
 
 class ExtraJinja2KeysCleanup(MiniMigrator):
-    # if you add a key here, you need to add it to the set of
-    # keys kept in the graph in make_graph.py
-    keys_to_remove = [
-        'min_r_ver',
-        'max_r_ver',
-        'min_py_ver',
-        'max_py_ver',
-        'compiler_stack',
-    ]
+    """Remove extra Jinja2 keys that make things hard for the version migrator.
+
+    Variables removed:
+
+    - ``hash_type``: This is sometimes used as a key in the ``source`` section
+      to map the hash_type (ex. sha256) to the hash.
+    - ``hash_value``: This is unnecessary since it is typically only used once
+      in a YAML recipe.
+    - ``file_ext``: This is unnecessary and just makes things harder to read.
+      It may also be causing trouble for the version migrator.
+
+    """
 
     def filter(self, attrs: "AttrsTypedDict", not_bad_str_start: str = "") -> bool:
-        """only remove the keys if they are there"""
-        print(attrs, not_bad_str_start)
+        """Check the raw YAML to find jinja variables to deal with."""
+        raw_yaml = attrs['raw_meta_yaml']
+        if '{% set file_ext' in raw_yaml or '{% set hash_type' in raw_yaml:
+            return False
         return True
-        # cfy = attrs.get("conda-forge.yml", {})
-        # if any(k in cfy for k in self.keys_to_remove):
-        #     return False
-        # else:
-        #     return True
+
+    def _replace_jinja_key(self, key_name, lines):
+        """Replace any usage of and the definition of a jinja2 variable."""
+        key_val = None
+        var_use = "{{ " + key_name + " }}"
+        var_def = "{% set " + key_name
+        for line in lines:
+            if line.startswith(var_def):
+                quote1_idx = line.find('"')
+                quote2_idx = line.rfind('"')
+                key_val = line[quote1_idx+1:quote2_idx]
+                continue
+            if key_val is not None:
+                # in a string literal
+                line = line.replace("'" + var_use + "'", key_val)
+                line = line.replace(var_use, key_val)
+            yield line
 
     def migrate(self, recipe_dir: str, attrs: "AttrsTypedDict", **kwargs: Any) -> None:
-        print(recipe_dir, attrs, kwargs)
-        # with indir(recipe_dir):
-        #     cfg_path = os.path.join('..', 'conda-forge.yml')
-        #     yaml = YAML()
-        #     yaml.indent(mapping=2, sequence=4, offset=2)
-        #
-        #     with open(cfg_path, 'r') as fp:
-        #         cfg = yaml.load(fp.read())
-        #
-        #     for k in self.keys_to_remove:
-        #         if k in cfg:
-        #             del cfg[k]
-        #
-        #     with open(cfg_path, 'w') as fp:
-        #         yaml.dump(cfg, fp)
+        with indir(recipe_dir):
+            with open('meta.yaml', 'r') as fp:
+                lines = fp.readlines()
+
+            lines = list(self._replace_jinja_key('file_ext', lines))
+            lines = list(self._replace_jinja_key('hash_type', lines))
+            lines = list(self._replace_jinja_key('hash_value', lines))
+
+            yaml = YAML(typ='jinja2')
+            yaml.indent(mapping=2, sequence=4, offset=2)
+            yaml.width = 120
+
+            # Rewrite the recipe file
+            with open('meta.yaml', 'w') as fp:
+                fp.writelines(lines)
--- tests/test_extra_jinja2_keys_migrator.py
+++ tests/test_extra_jinja2_keys_migrator.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 from ruamel.yaml import YAML
 
 from conda_forge_tick.migrators import (
@@ -14,14 +13,7 @@ VERSION_CF = Version(piggy_back_migrations=[ExtraJinja2KeysCleanup()])
 YAML_PATH = os.path.join(os.path.dirname(__file__), 'test_yaml')
 
 
-print("Importing!")
-@pytest.mark.parametrize('cases', [
-    tuple(),
-    # ('min_r_ver',),
-])
-def test_version_extra_jinja2_keys_cleanup(cases, tmpdir):
-    yaml = YAML()
-
+def test_version_extra_jinja2_keys_cleanup(tmpdir):
     with open(os.path.join(YAML_PATH, 'version_extra_jinja2_keys.yaml'), 'r') as fp:
         in_yaml = fp.read()
 
@@ -31,39 +23,17 @@ def test_version_extra_jinja2_keys_cleanup(cases, tmpdir):
     ) as fp:
         out_yaml = fp.read()
 
-    cf_yml = {}
-    print("############ cases: ", cases)
-    for case in cases:
-        cf_yml[case] = '10'
-    cf_yml['foo'] = 'bar'
-
     os.makedirs(os.path.join(tmpdir, 'recipe'), exist_ok=True)
-    cf_yml_pth = os.path.join(tmpdir, 'conda-forge.yml')
-    with open(cf_yml_pth, 'w') as fp:
-        yaml.dump(cf_yml, fp)
-
-    print("Before run_test_migration")
     run_test_migration(
         m=VERSION_CF,
         inp=in_yaml,
         output=out_yaml,
-        kwargs={"new_version": "0.19.2"},
+        kwargs={"new_version": "0.20.0"},
         prb="Dependencies have been updated if changed",
         mr_out={
             "migrator_name": "Version",
             "migrator_version": Version.migrator_version,
-            "version": "0.19.2",
+            "version": "0.20.0",
         },
         tmpdir=os.path.join(tmpdir, 'recipe'),
     )
-    print("After run_test_migration")
-
-    with open(cf_yml_pth, 'r') as fp:
-        new_cf_yml = yaml.load(fp)
-
-    assert 'min_r_ver' not in new_cf_yml
-    assert 'min_py_ver' not in new_cf_yml
-    assert 'max_r_ver' not in new_cf_yml
-    assert 'max_py_ver' not in new_cf_yml
-    assert 'compiler_stack' not in new_cf_yml
-    assert cf_yml['foo'] == 'bar'
