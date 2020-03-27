26d2eaf7e958826391c706882ed9fe101d21899f
Vinicius Douglas Cerutti <51954708+viniciusdc@users.noreply.github.com>
2020-03-27 13:15:58 -0300

    f-strings, black and bad/archived status
    Waiting for approval. I removed the verification for bad or achieved status and included f-strings format. (i was not using the _ all_nodes)

--- conda_forge_tick/migrate_update_version.py
+++ conda_forge_tick/migrate_update_version.py
@@ -1,8 +1,17 @@
 # type this code in at the interpreter
 from conda_forge_tick.utils import load_graph
-from conda_forge_tick.update_upstream_versions import get_latest_version, PyPI, CRAN, NPM, ROSDistro, RawURL, Github
+from conda_forge_tick.update_upstream_versions import (
+    get_latest_version,
+    PyPI,
+    CRAN,
+    NPM,
+    ROSDistro,
+    RawURL,
+    Github,
+)
 import random
 import json
+
 sources = (PyPI(), CRAN(), NPM(), ROSDistro(), RawURL(), Github())
 
 print("Reading graph")
@@ -11,33 +20,37 @@ _all_nodes = [t for t in gx.nodes.items()]
 random.shuffle(_all_nodes)
 
 # from here you can inspect the graph object, and what node will be updated:
-print("Number of nodes:", len(gx.nodes))
+print(f"Number of nodes: {len(gx.nodes)}")
 Node_count = 0
 
 to_update = {}
 to_update["nodes"] = []
-for node in gx.nodes:
-    with gx.nodes["{}".format(node)]["payload"] as attrs:
+for node, node_attrs in _all_nodes:
+    with node_attrs["payload"] as attrs:
         # verify the actual situation of the package;
+        actual_ver = str(attrs.get("version"))
         if attrs.get("bad") or attrs.get("archived"):
-            new_version_flag = False
-        
+            print(
+                f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - bad/archived"
+            )
+            pass
         # new version request
         try:
             new_version = get_latest_version(node, attrs, sources)
-            new_version = new_version or new_version_flag
         except Exception as e:
             try:
                 se = repr(e)
             except Exception as ee:
                 se = "Bad exception string: {}".format(ee)
-                print("warning: Error getting uptream version of {}: {}".format(node, se))
+                print(f"Warning: Error getting upstream version of {node}: {se}")
         else:
-            print("# {:<5} - {:<30} - ver: {:<10} - new ver: {}".format(Node_count, node, attrs.get('version'), str(new_version)))
-        
+            print(
+                f"# {Node_count:<5} - {node:<30} - ver: {attrs.get('version'):<10} - new ver: {new_version}"
+            )
+
         to_update["nodes"].append({"id": str(node), "version": str(new_version)})
     Node_count += 1
 
 print("writing out file")
-with open("new_version.json", 'w') as outfile:
+with open("new_version.json", "w") as outfile:
     json.dump(to_update, outfile)
