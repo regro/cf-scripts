1f074ea987bfeac61bb7e4191e99bd93f8f379a9
Christopher J. Wright <cjwright4242@gmail.com>
2020-04-02 21:55:57 -0400

    add test

--- conda_forge_tick/audit.py
+++ conda_forge_tick/audit.py
@@ -1,3 +1,4 @@
+import json
 import os
 import traceback
 
@@ -15,21 +16,13 @@ from conda_forge_tick.xonsh_utils import indir, env
 
 def audit_feedstock(fctx: FeedstockContext, ctx: MigratorSessionContext):
     """Uses Depfinder to audit the requirements for a python package
-
-    Parameters
-    ----------
-    feedstock_attrs
-
-    Returns
-    -------
-
     """
     # get feedstock
-    feedstock_dir = os.path.join(ctx.rever_dir,
-                                 fctx.package_name + "-feedstock")
+    feedstock_dir = os.path.join(ctx.rever_dir, fctx.package_name + "-feedstock")
     origin = feedstock_url(fctx=fctx)
-    fetch_repo(feedstock_dir=feedstock_dir, origin=origin, upstream=origin,
-               branch='master')
+    fetch_repo(
+        feedstock_dir=feedstock_dir, origin=origin, upstream=origin, branch="master"
+    )
     recipe_dir = os.path.join(feedstock_dir, "recipe")
 
     # get source code
@@ -44,34 +37,37 @@ def audit_feedstock(fctx: FeedstockContext, ctx: MigratorSessionContext):
 
 def main(args):
     gx = load_graph()
-    ctx = MigratorSessionContext('', '', '')
+    ctx = MigratorSessionContext("", "", "")
     start_time = time.time()
     # limit graph to things that depend on python
-    python_des = nx.descendants(gx, 'python')
+    python_des = nx.descendants(gx, "python")
     for node in sorted(
-            python_des, key=lambda x: (len(nx.descendants(gx, x)), x), reverse=True,
+        python_des, key=lambda x: (len(nx.descendants(gx, x)), x), reverse=True,
     ):
-        if (
-                time.time() - int(
-            env.get("START_TIME", start_time))
-                > int(env.get("TIMEOUT", 60))
+        if time.time() - int(env.get("START_TIME", start_time)) > int(
+            env.get("TIMEOUT", 60)
         ):
             break
         # depfinder only work on python at the moment so only work on things
         # with python as runtime dep
-        with gx.nodes[node]['payload'] as payload:
-            if not payload.get('archived', False) and 'python' in payload['requirements']['run'] and 'depfinder_audit' not in payload:
+        with gx.nodes[node]["payload"] as payload:
+            if (
+                not payload.get("archived", False)
+                and "python" in payload["requirements"]["run"]
+                and "depfinder_audit" not in payload
+            ):
                 print(node)
-                fctx = FeedstockContext(package_name=node,
-                                        feedstock_name=payload['name'],
-                                        attrs=payload)
+                fctx = FeedstockContext(
+                    package_name=node, feedstock_name=payload["name"], attrs=payload
+                )
                 try:
                     deps = audit_feedstock(fctx, ctx)
                 except Exception as e:
-                    deps = {"exception": str(e),
-                            "traceback": str(traceback.format_exc()).split("\n"),}
+                    deps = {
+                        "exception": str(e),
+                        "traceback": str(traceback.format_exc()).split("\n"),
+                    }
                 finally:
-                    os.makedirs('audits', exist_ok=True)
-                    with open(f'audits/{node}.json', 'w') as f:
+                    os.makedirs("audits", exist_ok=True)
+                    with open(f"audits/{node}.json", "w") as f:
                         json.dump(deps, f)
-
