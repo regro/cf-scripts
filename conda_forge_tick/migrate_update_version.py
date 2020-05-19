b77ad435761c78f5800be1c939b877b6e7e9ae9a
viniciusdc <vinivdc2009@hotmail.com>
2020-05-19 08:59:14 -0300

    new scheme for migrate_update_function

--- conda_forge_tick/migrate_update_version.py
+++ conda_forge_tick/migrate_update_version.py
@@ -1,4 +1,3 @@
-# type this code in at the interpreter
 from conda_forge_tick.utils import load_graph
 from conda_forge_tick.update_upstream_versions import (
     get_latest_version,
@@ -14,18 +13,19 @@ import json
 
 sources = (PyPI(), CRAN(), NPM(), ROSDistro(), RawURL(), Github())
 
-print("Reading graph")
+# Reading graph
 gx = load_graph()
 _all_nodes = [t for t in gx.nodes.items()]
 random.shuffle(_all_nodes)
 
-# from here you can inspect the graph object, and what node will be updated:
-print(f"Number of nodes: {len(gx.nodes)}")
+# Inspection the graph object and node update:
+# print(f"Number of nodes: {len(gx.nodes)}")
 Node_count = 0
 
 to_update = {}
 to_update["nodes"] = []
 for node, node_attrs in _all_nodes:
+    # checking each node
     with node_attrs["payload"] as attrs:
         # verify the actual situation of the package;
         actual_ver = str(attrs.get("version"))
@@ -33,8 +33,9 @@ for node, node_attrs in _all_nodes:
             print(
                 f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - bad/archived"
             )
+            Node_count += 1
             continue
-        # new version request
+        # New verison request
         try:
             new_version = get_latest_version(node, attrs, sources)
         except Exception as e:
@@ -47,10 +48,9 @@ for node, node_attrs in _all_nodes:
             print(
                 f"# {Node_count:<5} - {node:<30} - ver: {attrs.get('version'):<10} - new ver: {new_version}"
             )
-
         to_update["nodes"].append({"id": str(node), "version": str(new_version)})
     Node_count += 1
 
-print("writing out file")
+# Writing  out file
 with open("new_version.json", "w") as outfile:
     json.dump(to_update, outfile)
