fab2f148814996490904f6327b5bf4964bf925d9
viniciusdc <vinivdc2009@hotmail.com>
2020-06-09 13:52:04 -0300

    changes at the output file

--- conda_forge_tick/new_update_versions.py
+++ conda_forge_tick/new_update_versions.py
@@ -3,8 +3,9 @@ import logging
 import random
 import json
 import os
+import tqdm
 
-from conda_forge_tick.utils import setup_logger, load_graph
+from utils import setup_logger, load_graph
 from update_sources import (
     AbstractSource,
     PyPI,
@@ -24,7 +25,7 @@ from typing import (
 )
 
 # conda_forge_tick :: cft
-logger = logging.getLogger("cft._update_versions")
+logger = logging.getLogger("conda-forge-tick._update_versions")
 
 
 def get_latest_version(
@@ -43,8 +44,10 @@ def get_latest_version(
                 return ver
             else:
                 logger.debug(f"Upstream: Could not find version on {source.name}")
+                meta["bad"] = f"Upstream: Could not find version on {source.name}"
         if not meta_yaml.get("bad"):
             logger.debug("Upstream: unknown source")
+            meta["bad"] = "Upstream: unknown source"
         return False
 
 
@@ -68,28 +71,38 @@ def new_update_upstream_versions(
     # print(f"Number of nodes: {len(gx.nodes)}")
     Node_count = 0
 
-    to_update = {}
-    to_update["nodes"] = []
-    for node, node_attrs in _all_nodes:
+    to_update = []
+    for node, node_attrs in tqdm.tqdm(_all_nodes):
+        attrs = node_attrs["payload"]
+        if attrs.get("bad") or attrs.get("archived"):
+            attrs["new_version"] = False
+            continue
+        to_update.append((node, attrs))
+
+    up_to = {}
+    for node, node_attrs in to_update:
         # checking each node
-        with node_attrs["payload"] as attrs:
-            # rude exception
-            if node == "ca-policy-lcg":
-                to_update["nodes"].append({"id": str(node), "version": "False"})
-                Node_count += 1
-                continue
+        with node_attrs as attrs:
+            up_to = {}
 
             # verify the actual situation of the package;
             actual_ver = str(attrs.get("version"))
-            if attrs.get("bad") or attrs.get("archived"):
-                logger.info(
-                    f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - bad/archived"
-                )
+
+            # rude exception
+            if node == "ca-policy-lcg":
+                up_to["ca-policy-lcg"] = {
+                    "bad": attrs.get("bad"),
+                    "new_version": False,
+                    "new_version_attempts": attrs.get("new_version_attempts"),
+                    "new_version_errors": attrs.get("new_version_errors")
+                }
                 Node_count += 1
                 continue
+
             # New verison request
             try:
                 new_version = get_latest_version(node, attrs, sources)
+                attrs["new_version"] = new_version or attrs["new_version"]
             except Exception as e:
                 try:
                     se = repr(e)
@@ -98,17 +111,22 @@ def new_update_upstream_versions(
                 logger.warning(
                     f"Warning: Error getting upstream version of {node}: {se}"
                 )
-
-            logger.info(
-                f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - new ver: {new_version}"
-            )
-            to_update["nodes"].append({"id": str(node), "version": str(new_version)})
+                attrs["bad"] = "Upstream: Error getting upstream version"
+            else:
+                logger.info(
+                    f"# {Node_count:<5} - {node} - {attrs.get('version')} - {attrs.get('new_version')}",
+                )
+            up_to[f"{node}"] = {
+                "bad": attrs.get("bad"),
+                "new_version": attrs.get("new_version"),
+                "new_version_attempts": attrs.get("new_version_attempts"),
+                "new_version_errors": attrs.get("new_version_errors")
+            }
             Node_count += 1
-    return to_update
+    return up_to
 
 
 def main(args: Any = None) -> None:
-    logger.info("cft :: conda_forge_tick")
     if CONDA_FORGE_TICK_DEBUG:
         setup_logger(logger, level="debug")
     else:
