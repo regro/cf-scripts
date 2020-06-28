22ecca3ff58022ef4484c5c15a33de3d11514088
viniciusdc <vinivdc2009@hotmail.com>
2020-06-28 08:53:57 -0300

    attempt to rm node writing

--- conda_forge_tick/new_update_versions.py
+++ conda_forge_tick/new_update_versions.py
@@ -30,7 +30,7 @@ logger = logging.getLogger("conda-forge-tick._update_versions")
 
 
 def get_latest_version(
-        name: str, payload_meta_yaml: Any, sources: Iterable[AbstractSource],
+        name: str, payload_meta_yaml: Any, sources: Iterable[AbstractSource], versions: dict
 ):
     with payload_meta_yaml as meta_yaml:
         for source in sources:
@@ -45,10 +45,10 @@ def get_latest_version(
                 return ver
             else:
                 logger.debug(f"Upstream: Could not find version on {source.name}")
-                meta_yaml["bad"] = f"Upstream: Could not find version on {source.name}"
+                versions["bad"] = f"Upstream: Could not find version on {source.name}"
         if not meta_yaml.get("bad"):
             logger.debug("Upstream: unknown source")
-            meta_yaml["bad"] = "Upstream: unknown source"
+            versions["bad"] = "Upstream: unknown source"
         return False
 
 
@@ -93,7 +93,7 @@ def new_update_upstream_versions(
             # New version request
             try:
                 # check for latest version
-                new_version = get_latest_version(node, attrs, sources) or attrs["new_version"]
+                new_version = get_latest_version(node, attrs, sources, up_to) or attrs["new_version"]
                 up_to['new_version'] = new_version
             except Exception as e:
                 try:
