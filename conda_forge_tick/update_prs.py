9caa9328d97601a1954c0fbc6635c0f48061916b
viniciusdc <vinivdc2009@hotmail.com>
2020-08-14 21:18:24 -0300

    context manager idea

--- conda_forge_tick/update_prs.py
+++ conda_forge_tick/update_prs.py
@@ -182,25 +182,25 @@ def profiling():
     def _profiling(f):
         @wraps(f)
         def __profiling(*rgs, **kwargs):
-            prof = cProfile.Profile()
-            prof.enable()
+            with cProfile.Profile() as prof:
+                prof = cProfile.Profile()
+                prof.enable()
 
-            # out_result = f(*rgs, **kwargs) should we expect an output ?
-            f(*rgs, **kwargs)
+                out_result = f(*rgs, **kwargs)
 
-            prof.disable()
+                prof.disable()
 
-            # get current time
-            now = datetime.now()
-            current_time = now.strftime("%d-%m-%Y") + "_" + now.strftime("%H_%M_%S")
-            # process name -- aka profiler sub-folder
-            process_name = os.path.basename(__file__)
-            # check dir
-            os.makedirs(f"profiler/{process_name}", exist_ok=True)
-            # save stats into file
-            prof.dump_stats(f"profiler/{process_name}/{current_time}")
+                # get current time
+                now = datetime.now()
+                current_time = now.strftime("%d-%m-%Y") + "_" + now.strftime("%H_%M_%S")
+                # process name -- aka profiler sub-folder
+                process_name = os.path.basename(__file__)
+                # check dir
+                os.makedirs(f"profiler/{process_name}", exist_ok=True)
+                # save stats into file
+                prof.dump_stats(f"profiler/{process_name}/{current_time}")
 
-            return  # out_result
+                return out_result
 
         return __profiling
 
