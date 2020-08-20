e0803fb63ecd31108ae5721202e9bdbd2828a67e
viniciusdc <vinivdc2009@hotmail.com>
2020-08-20 18:18:04 -0300

    NEW profiler decorator

--- conda_forge_tick/profiler.py
+++ conda_forge_tick/profiler.py
@@ -1,6 +1,11 @@
 from cProfile import Profile
 from contextlib import contextmanager
 
+import os
+import inspect
+from functools import wraps
+from datetime import datetime
+
 
 class Profiled(Profile):
     # extend init case for cProfile.Profile
@@ -13,3 +18,26 @@ class Profiled(Profile):
         self.enable()
         yield  # Execute code to be profiled.
         self.disable()
+
+
+def profiling(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        # Profile dump information
+        # get current time
+        now = datetime.now()
+        current_time = now.strftime("%d-%m-%Y") + "_" + now.strftime("%H_%M_%S")
+        # process name -- aka profiler sub-folder
+        process_name = os.path.basename(inspect.getfile(f)).replace('.py', '')
+        # check dir
+        os.makedirs(f"profiler/{process_name}", exist_ok=True)
+
+        profiled = Profiled()  # Create class instance.
+        with profiled():
+            out = f(*args, **kwargs)
+        # save stats into file
+        profiled.dump_stats(f"profiler/{process_name}/{current_time}")
+
+        return out
+
+    return wrapper
