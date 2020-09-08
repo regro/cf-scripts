583452da7d2fb9c026fffd184afa431a5050bbec
viniciusdc <vinivdc2009@hotmail.com>
2020-09-08 13:30:54 -0300

    attempt to solve the folder creation issue

--- conda_forge_tick/profiler.py
+++ conda_forge_tick/profiler.py
@@ -20,24 +20,27 @@ class Profiled(Profile):
         self.disable()
 
 
-def profiling(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        # Profile dump information
-        # get current time
-        now = datetime.now()
-        current_time = now.strftime("%d-%m-%Y") + "_" + now.strftime("%H_%M_%S")
-        # process name -- aka profiler sub-folder
-        process_name = os.path.basename(inspect.getfile(f)).replace(".py", "")
-        # check dir
-        os.makedirs(f"profiler/{process_name}", exist_ok=True)
+def profiling(name: str = None):
+    def _profiling(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            # Profile dump information
+            # get current time
+            now = datetime.now()
+            current_time = now.strftime("%d-%m-%Y") + "_" + now.strftime("%H_%M_%S")
+            # process name -- aka profiler sub-folder
+            process_name = name
+            # check dir
+            os.makedirs(f"profiler/{process_name}", exist_ok=True)
 
-        profiled = Profiled()  # Create class instance.
-        with profiled():
-            out = f(*args, **kwargs)
-        # save stats into file
-        profiled.dump_stats(f"profiler/{process_name}/{current_time}")
+            profiled = Profiled()  # Create class instance.
+            with profiled():
+                out = f(*args, **kwargs)
+            # save stats into file
+            profiled.dump_stats(f"profiler/{process_name}/{current_time}")
 
-        return out
+            return out
 
-    return wrapper
+        return wrapper
+
+    return _profiling
