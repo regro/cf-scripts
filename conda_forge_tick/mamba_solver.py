ef04fdf7dcf6bf9a4b4fdaae69a4ac05299ecd40
Matthew R. Becker <becker.mr@gmail.com>
2020-10-26 15:30:30 -0500

    ENH more processes

--- conda_forge_tick/mamba_solver.py
+++ conda_forge_tick/mamba_solver.py
@@ -382,7 +382,7 @@ class MambaSolver:
         dict with the weak and strong run exports for the packages.
         """
 
-        with ProcessPoolExecutor(max_workers=1) as exe:
+        with ProcessPoolExecutor(max_workers=None) as exe:
             futures = []
             for link_tuple in link_tuples:
                 if link_tuple not in self.run_exports:
