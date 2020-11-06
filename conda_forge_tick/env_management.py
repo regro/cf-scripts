3c354f0019faa35f9f9e2cca06b316387b17b1a8
Christopher J. Wright <cjwright4242@gmail.com>
2020-11-06 13:58:22 -0500

    one more comment

--- conda_forge_tick/env_management.py
+++ conda_forge_tick/env_management.py
@@ -9,6 +9,6 @@ SENSITIVE_ENVIRON = {k: os.environ.pop(k, None) for k in SENSITIVE_KEYS}
 def sensitive_env():
     """Add sensitive keys to environ if needed, when ctx is finished remove keys and update the sensitive env
     in case any were updated inside the ctx"""
-    # os.environ.update(**SENSITIVE_ENVIRON)
+    os.environ.update(**SENSITIVE_ENVIRON)
     yield os.environ
     SENSITIVE_ENVIRON.update({k: os.environ.pop(k, None) for k in list(SENSITIVE_ENVIRON)})
