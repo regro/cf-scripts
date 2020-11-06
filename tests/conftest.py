bf587a6745a9879ebcc94d1e59fc1a78082908a2
Matthew R. Becker <becker.mr@gmail.com>
2020-11-06 16:26:48 -0600

    BUG conftest should not hide when in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,16 +6,21 @@ from conda_forge_tick import global_sensitive_env
 
 @pytest.fixture
 def env_setup():
-    old_pwd = os.environ.pop("PASSWORD", None)
-    os.environ["PASSWORD"] = os.environ.get("TEST_PASSWORD_VAL", "unpassword")
+
+    if "TEST_PASSWORD_VAL" not in os.environ:
+        old_pwd = os.environ.pop("PASSWORD", None)
+        os.environ["PASSWORD"] = "unpassword"
+        global_sensitive_env.hide_env_vars()
+
     old_pwd2 = os.environ.pop("pwd", None)
     os.environ["pwd"] = "pwd"
-    if "TEST_PASSWORD_VAL" not in os.environ:
-        global_sensitive_env.hide_env_vars()
+
     yield
+
     if "TEST_PASSWORD_VAL" not in os.environ:
         global_sensitive_env.reveal_env_vars()
-    if old_pwd:
-        os.environ["PASSWORD"] = old_pwd
+        if old_pwd:
+            os.environ["PASSWORD"] = old_pwd
+
     if old_pwd2:
         os.environ["pwd"] = old_pwd2
