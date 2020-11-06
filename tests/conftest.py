eab3c97b013ed2ab3648724cd029ddfbbc35c282
Matthew R. Becker <becker.mr@gmail.com>
2020-11-06 16:09:56 -0600

    ENH make it work in both configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from conda_forge_tick import global_sensitive_env
 @pytest.fixture
 def env_setup():
     old_pwd = os.environ.pop("PASSWORD", None)
-    os.environ["PASSWORD"] = "unpassword"
+    os.environ["PASSWORD"] = os.environ.get("TEST_PASSWORD_VAL", "unpassword")
     old_pwd2 = os.environ.pop("pwd", None)
     os.environ["pwd"] = "pwd"
     global_sensitive_env.hide_env_vars()
