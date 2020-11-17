f23778ed5b98432e01d649f17e98f501dbcfeb27
Matthew R. Becker <becker.mr@gmail.com>
2020-11-17 12:59:43 -0600

    BUG do not need nodes

--- conda_forge_tick/auto_tick.py
+++ conda_forge_tick/auto_tick.py
@@ -566,7 +566,7 @@ def migration_factory(
         ]
 
     output_to_feedstock = gx.graph["outputs_lut"]
-    all_package_names = set(gx.nodes) | set(
+    all_package_names = set(
         sum(
             [
                 list(node.get("payload", {}).get("outputs_names", set()))
