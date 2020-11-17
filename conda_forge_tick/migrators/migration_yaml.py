b8d2079b66125906c0632b7b33855ea53344ec49
Christopher J. Wright <cjwright4242@gmail.com>
2020-11-17 16:45:48 -0500

    fix syntax issues

--- conda_forge_tick/migrators/migration_yaml.py
+++ conda_forge_tick/migrators/migration_yaml.py
@@ -472,9 +472,8 @@ def create_rebuild_graph(
     total_graph = copy.deepcopy(gx)
     excluded_feedstocks = set() if excluded_feedstocks is None else excluded_feedstocks
     # Always exclude the packages themselves from the migration
-    excluded_feedstocks.update(
-        {gx.graph["outputs_lut"].get(node, node) for node in package_names},
-    )
+    for node in package_names:
+        excluded_feedstocks.update(gx.graph["outputs_lut"].get(node, node))
 
     included_nodes = set()
 
