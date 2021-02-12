70cd50245727b7fd9a8196e48a0f86d216dbae9b
Matthew R. Becker <becker.mr@gmail.com>
2021-02-12 14:36:51 -0600

    ENH more tests

--- tests/test_url_exists.py
+++ tests/test_url_exists.py
@@ -13,6 +13,26 @@ from conda_forge_tick.update_sources import url_exists
             True,
         ),
         ("https://eups.lsst.codes/stack/src/tags/w_2021_07.list", True),
+        (
+            "https://downloads.sourceforge.net/project/healpix/Healpix_3.31/Healpix_3.31_2016Aug26.tar.gz",  # noqa
+            True,
+        ),
+        (
+            "https://downloads.sourceforge.net/project/healpix/Healpix_3.345/Healpix_3.345_2016Aug26.tar.gz",  # noqa
+            False,
+        ),
+        (
+            "http://spams-devel.gforge.inria.fr/hitcounter2.php?file/38351/spams-2.6.2.5.tar.gz",  # noqa
+            True,
+        ),
+        (
+            "https://github.com/Kitware/CMake/releases/download/v3.19.4/cmake-3.19.4.tar.gz",  # noqa
+            True,
+        ),
+        (
+            "https://github.com/Kitware/CMake/releases/download/v3.19.4435784/cmake-3.19.4435784.tar.gz",  # noqa
+            False,
+        ),
     ],
 )
 def test_url_exists(url, exists):
