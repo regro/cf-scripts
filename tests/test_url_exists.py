b2db9f1253792567f68a90fa4878b2bc9f9e9419
Matthew R. Becker <becker.mr@gmail.com>
2021-02-12 12:37:05 -0600

    blacken

--- tests/test_url_exists.py
+++ tests/test_url_exists.py
@@ -3,11 +3,17 @@ import pytest
 from conda_forge_tick.update_sources import url_exists
 
 
-@pytest.mark.parametrize("url,exists", [
-    ("https://github.com/beckermr/pizza-cutter/archive/0.2.2.tar.gz", True)
-    ("https://github.com/beckermr/pizza-cutter/archive/0.2.3454794.tar.gz", False),
-    ("http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.6p1.tar.gz", True),
-    ("https://eups.lsst.codes/stack/src/tags/w_2021_07.list", True),
-])
+@pytest.mark.parametrize(
+    "url,exists",
+    [
+        ("https://github.com/beckermr/pizza-cutter/archive/0.2.2.tar.gz", True),
+        ("https://github.com/beckermr/pizza-cutter/archive/0.2.3454794.tar.gz", False),
+        (
+            "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.6p1.tar.gz",
+            True,
+        ),
+        ("https://eups.lsst.codes/stack/src/tags/w_2021_07.list", True),
+    ],
+)
 def test_url_exists(url, exists):
     assert url_exists(url) is exists
