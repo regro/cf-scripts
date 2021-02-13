45605dbaa293cb76a8b83d841575069d7a79d062
Matthew R. Becker <becker.mr@gmail.com>
2021-02-13 06:49:24 -0600

    TST added tests for url hashing

--- tests/test_upstream_versions.py
+++ tests/test_upstream_versions.py
@@ -1,7 +1,8 @@
 import pytest
 
+from conda.models.version import VersionOrder
 from conda_forge_tick.update_upstream_versions import get_latest_version
-from conda_forge_tick.update_sources import NPM, next_version
+from conda_forge_tick.update_sources import NPM, next_version, RawURL
 from conda_forge_tick.utils import parse_meta_yaml, LazyJson
 
 sample_npm = """
@@ -146,23 +147,189 @@ sample_npm_response = """
 }
 """  # noqa
 
+sample_spams = """\
+{% set version = "2.6.1" %}
+{% set date = "2017-12-08" %}
+{% set file_num = "37237" %}
+
+
+package:
+  name: python-spams
+  version: {{ version }}
+
+source:
+  fn: spams-python-{{ version }}.tar.gz
+  url: http://spams-devel.gforge.inria.fr/hitcounter2.php?file={{ file_num }}/spams-python-v{{ version }}-svn{{ date }}.tar.gz
+  sha256: 3012529c0461bd777228dce53c880f2a5fa2aaf22b5d673a1f22045a63f4d8bc
+
+  patches:
+    # patch setup.py to link to LLVM's OpenMP library instead of GCC's.
+    - llvm_openmp_setup.py.patch  # [osx]
+    # array creation requires integers for shape specification.
+    - spams.py.patch
+
+build:
+  skip: true  # [win or (osx and cxx_compiler == "toolchain_cxx")]
+  number: 1204
+  detect_binary_files_with_prefix: true
+  features:
+
+requirements:
+  build:
+    - {{ compiler("cxx") }}
+    - llvm-openmp                    # [osx]
+  host:
+    - libblas
+    - liblapack
+    - python
+    - pip
+    - llvm-openmp                    # [osx]
+    - numpy
+  run:
+    - python
+    - llvm-openmp                    # [osx]
+    - {{ pin_compatible('numpy') }}
+    - scipy
+    - six
+
+test:
+  source_files:
+    # Test data
+    - extdata/boat.png
+    - extdata/lena.png
+    # Test suite
+    - test_decomp.py
+    - test_dictLearn.py
+    - test_linalg.py
+    - test_prox.py
+    - test_spams.py
+    - test_utils.py
+  requires:
+    - pillow
+  imports:
+    - spams
+
+about:
+  home: http://spams-devel.gforge.inria.fr/
+  license: GPL 3
+  license_family: GPL
+  license_file: LICENSE.txt
+  summary: An optimization toolbox for solving various sparse estimation problems.
+
+extra:
+  recipe-maintainers:
+    - jakirkham
+    - zym1010
+"""  # noqa
+
+sample_cmake_no_system = """\
+{% set version = "3.16.4" %}
+
+package:
+  name: cmake-no-system
+  version: {{ version }}
+
+source:
+  url: https://github.com/Kitware/CMake/releases/download/v{{ version }}/cmake-{{ version }}.tar.gz
+  sha256: 9bcc8c114d9da603af9512083ed7d4a39911d16105466beba165ba8fe939ac2c
+  patches:
+    - patches/3.16.2/0001-find_-Add-debug-logging-infrastructure.patch
+    - patches/3.16.2/0002-find_-Use-debug-logging-infrastructure.patch
+    - patches/3.16.2/0003-Add-more-debug-logging-to-cmFindCommon.patch
+
+build:
+  number: 0
+  skip: True  # [win]
+
+requirements:
+  build:
+    - {{ compiler('c') }}
+    - {{ compiler('cxx') }}
+    - make
+    - patch
+
+test:
+  commands:
+    - cmake --version
+    - ctest --version
+    - cpack --version
+
+about:
+  home: http://www.cmake.org/
+  license: BSD-3-Clause
+  license_family: BSD
+  license_file:
+    - Copyright.txt
+    - Utilities/cmbzip2/LICENSE
+    - Utilities/cmcurl/COPYING
+    - Utilities/cmexpat/COPYING
+    - Utilities/cmjsoncpp/LICENSE
+    - Utilities/cmlibarchive/COPYING
+    - Utilities/cmliblzma/COPYING
+    - Utilities/cmlibrhash/COPYING
+    - Utilities/cmlibuv/LICENSE
+    - Utilities/cmzlib/Copyright.txt
+    - Utilities/cmzstd/LICENSE
+  summary: CMake built without system libraries for use when building CMake dependencies.
+
+extra:
+  recipe-maintainers:
+    - jjhelmus
+    - beckermr
+"""  # noqa
+
 latest_url_test_list = [
     (
+        "configurable-http-proxy",
         sample_npm,
+        "3.1.0",
         "3.1.1",
         NPM(),
         {"https://registry.npmjs.org/configurable-http-proxy": sample_npm_response},
     ),
+    (
+        "python-spams",
+        sample_spams,
+        "2.6.1",
+        False,
+        RawURL(),
+        {},
+    ),
+    (
+        "cmake-no-system",
+        sample_cmake_no_system,
+        "3.16.4",
+        # represents any version in the test
+        # which exact one we get changes due to internet failures
+        None,
+        RawURL(),
+        {},
+    ),
 ]
 
 
-@pytest.mark.parametrize("inp, ver, source, urls", latest_url_test_list)
-def test_latest_version(inp, ver, source, urls, requests_mock, tmpdir):
+@pytest.mark.parametrize("name, inp, curr_ver, ver, source, urls", latest_url_test_list)
+def test_latest_version(name, inp, curr_ver, ver, source, urls, requests_mock, tmpdir):
     pmy = LazyJson(tmpdir.join("cf-scripts-test.json"))
     pmy.update(parse_meta_yaml(inp)["source"])
+    pmy.update(
+        {
+            "feedstock_name": name,
+            "version": curr_ver,
+            "raw_meta_yaml": inp,
+            "meta_yaml": parse_meta_yaml(inp),
+        },
+    )
     [requests_mock.get(url, text=text) for url, text in urls.items()]
-    attempt = get_latest_version("configurable-http-proxy", pmy, [source])
-    assert ver == attempt["new_version"]
+    attempt = get_latest_version(name, pmy, [source])
+    if ver is None:
+        assert not (attempt["new_version"] is False)
+        assert attempt["new_version"] != curr_ver
+        assert VersionOrder(attempt["new_version"]) > VersionOrder(curr_ver)
+    elif ver is False:
+        assert attempt["new_version"] is ver
+    else:
+        assert ver == attempt["new_version"]
 
 
 @pytest.mark.parametrize(
