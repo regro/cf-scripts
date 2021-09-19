13d6333720b51cc8befb19c726c165e922c57b14
Matthew R. Becker <becker.mr@gmail.com>
2021-09-19 07:25:04 -0500

    ENH add PRs for broken packages

--- conda_forge_tick/migrators/__init__.py
+++ conda_forge_tick/migrators/__init__.py
@@ -27,3 +27,4 @@ from .cross_compile import (
 from .duplicate_lines import DuplicateLinesCleanup
 from .cos7 import Cos7Config
 from .pip_wheel_dep import PipWheelMigrator
+from .broken_rebuild import RebuildBroken
