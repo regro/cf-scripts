e17d5110ecc608d53154bacd1a80ef951a6677d8
Matthew R. Becker <becker.mr@gmail.com>
2022-04-04 21:53:48 -0500

    BUG parse mpi run as build by hand

--- conda_forge_tick/migrators/mpi_pin_run_as_build.py
+++ conda_forge_tick/migrators/mpi_pin_run_as_build.py
@@ -9,6 +9,73 @@ from conda_forge_tick.migrators.core import MiniMigrator
 MPIS = ["mpich", "openmpi"]
 
 
+def _parse_cbc_mpi(lines):
+    in_prab = False
+    prab_indent = None
+    mpi_indent = None
+    new_lines = []
+    for _line in lines:
+        if _line.endswith("\n"):
+            _line = _line[:-1]
+        line = _line.split("#", 1)[0]
+
+        if len(line.strip()) > 0:
+            if '\t' in line:
+                line = line.replace('\t', '    ')
+
+            curr_indent = len(line) - len(line.lstrip())
+
+            if "pin_run_as_build" in line:
+                in_prab = True
+            elif in_prab:
+                if mpi_indent is not None:
+                    if curr_indent > mpi_indent:
+                        continue
+                    else:
+                        mpi_indent = None
+
+                if mpi_indent is None:
+                    for mpi in MPIS:
+                        if mpi + ":" in line:
+                            mpi_indent = len(line) - len(line.lstrip())
+                            break
+
+                    if mpi_indent is not None:
+                        continue
+
+                if prab_indent is None:
+                    prab_indent = len(line) - len(line.lstrip())
+                elif curr_indent <= prab_indent:
+                    in_prab = False
+                    prab_indent = None
+
+        new_lines.append(_line)
+
+    iprab = None
+    for i, ln in enumerate(new_lines):
+        if "pin_run_as_build" in ln:
+            iprab = i
+            break
+    if iprab is not None:
+        prab_indent = len(new_lines[iprab]) - len(new_lines[iprab].lstrip())
+        inext = None
+        if iprab < len(new_lines)-1:
+            for i in range(iprab+1, len(new_lines)):
+                if len(new_lines[i].split("#", 1)[0].strip()) > 0:
+                    inext = i
+                    break
+
+        if inext is not None:
+            next_prab_indent = len(new_lines[inext]) - len(new_lines[inext].lstrip())
+        else:
+            next_prab_indent = prab_indent
+
+        if prab_indent == next_prab_indent:
+            new_lines.pop(iprab)
+
+    return new_lines
+
+
 class MPIPinRunAsBuildCleanup(MiniMigrator):
     def filter(self, attrs, not_bad_str_start=""):
         host_req = (attrs.get("requirements", {}) or {}).get("host", set()) or set()
