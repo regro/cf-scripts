0691807d853fa4e1fbe957019edc617afd86d13b
Isuru Fernando <isuruf@gmail.com>
2022-06-01 17:02:38 -0500

    fix migrator

--- conda_forge_tick/migrators/qt_to_qt_main.py
+++ conda_forge_tick/migrators/qt_to_qt_main.py
@@ -5,24 +5,24 @@ import os
 def _parse_qt(lines):
     new_lines = []
     for line in lines:
-        if line.endswith(" qt"):
+        if line.endswith(" qt\n"):
             line = line.replace(" qt", " qt-main")
         line = line.replace(" qt ", " qt-main ")
         new_lines.append(line)
+    return new_lines
 
 
 class QtQtMainMigrator(MiniMigrator):
     def filter(self, attrs, not_bad_str_start=""):
         host_req = (attrs.get("requirements", {}) or {}).get("host", set()) or set()
-        return "qt" in host_req
+        return "qt" not in host_req
 
     def migrate(self, recipe_dir, attrs, **kwargs):
-        fname = os.path.join(recipe_dir, "conda_build_config.yaml")
+        fname = os.path.join(recipe_dir, "meta.yaml")
         if os.path.exists(fname):
-
             with open(fname) as fp:
                 lines = fp.readlines()
 
             new_lines = _parse_qt(lines)
             with open(fname, "w") as fp:
-                fp.write("\n".join(new_lines))
+                fp.write("".join(new_lines))
