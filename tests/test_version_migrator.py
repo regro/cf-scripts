bad625c90d0b7b4227caea48abb572b5d8a807af
beckermr <becker.mr@gmail.com>
2022-08-01 06:38:49 -0500

    TST added test for dbt

--- tests/test_version_migrator.py
+++ tests/test_version_migrator.py
@@ -15,6 +15,7 @@ YAML_PATH = os.path.join(os.path.dirname(__file__), "test_yaml")
 @pytest.mark.parametrize(
     "case,new_ver",
     [
+        ("dbt", "1.2.0"),
         ("jinja2expr", "1.1.1"),
         ("weird", "1.6.0"),
         ("compress", "0.9"),
