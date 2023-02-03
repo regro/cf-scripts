c6ac11ab4bf2cb9ab7c99ee948c1db0accdd7dc4
Eric Dill <vcs@ericdill.dev>
2023-02-03 11:01:42 -0500

    back out unnecessary changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,10 @@ import os
 import pytest
 from conda_forge_tick import global_sensitive_env
 
-from flaky import flaky
-
 
 @pytest.fixture
 def env_setup():
+
     if "TEST_PASSWORD_VAL" not in os.environ:
         old_pwd = os.environ.pop("PASSWORD", None)
         os.environ["PASSWORD"] = "unpassword"
