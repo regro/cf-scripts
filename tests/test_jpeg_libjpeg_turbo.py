e59916d07202ae04a35b8f9c2c5dce0474b1e8bd
Mark Harfouche <mark.harfouche@gmail.com>
2023-02-11 18:40:33 -0500

    Add a piggy back migration for jpeg turbo

---
+++ tests/test_jpeg_libjpeg_turbo.py
@@ -0,0 +1,46 @@
+import os
+import pytest
+
+from conda_forge_tick.migrators import JpegTurboMigrator, Version
+from test_migrators import run_test_migration
+
+JPEGJPEGTURBO = JpegTurboMigrator()
+VERSION_WITH_QTQTMAIN = Version(
+    set(),
+    piggy_back_migrations=[JPEGJPEGTURBO],
+)
+
+YAML_PATH = os.path.join(os.path.dirname(__file__), "test_yaml")
+
+
+@pytest.mark.parametrize(
+    "old_meta,new_meta,new_ver",
+    [
+        (
+            "jpegturbo_r_base_before.yaml",
+            "jpegturbo_r_base_after.yaml",
+            "4.2.2",
+        ),
+    ],
+)
+def test_qt_main(old_meta, new_meta, new_ver, tmpdir):
+    with open(os.path.join(YAML_PATH, old_meta)) as fp:
+        in_yaml = fp.read()
+
+    with open(os.path.join(YAML_PATH, new_meta)) as fp:
+        out_yaml = fp.read()
+
+    run_test_migration(
+        m=VERSION_WITH_QTQTMAIN,
+        inp=in_yaml,
+        output=out_yaml,
+        kwargs={"new_version": new_ver},
+        prb="Dependencies have been updated if changed",
+        mr_out={
+            "migrator_name": "Version",
+            "migrator_version": VERSION_WITH_QTQTMAIN.migrator_version,
+            "version": new_ver,
+        },
+        tmpdir=tmpdir,
+        should_filter=False,
+    )
