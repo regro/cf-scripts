207a75150f40b96ecbeb5a0c0b2a88de75c1eb97
Mark Harfouche <mark.harfouche@gmail.com>
2023-02-11 19:03:54 -0500

    Not sure what is wrong

--- tests/test_jpeg_libjpeg_turbo.py
+++ tests/test_jpeg_libjpeg_turbo.py
@@ -5,7 +5,7 @@ from conda_forge_tick.migrators import JpegTurboMigrator, Version
 from test_migrators import run_test_migration
 
 JPEGJPEGTURBO = JpegTurboMigrator()
-VERSION_WITH_QTQTMAIN = Version(
+VERSION_WITH_JPEGTURBO = Version(
     set(),
     piggy_back_migrations=[JPEGJPEGTURBO],
 )
@@ -23,7 +23,7 @@ YAML_PATH = os.path.join(os.path.dirname(__file__), "test_yaml")
         ),
     ],
 )
-def test_qt_main(old_meta, new_meta, new_ver, tmpdir):
+def test_jpeg_turbo(old_meta, new_meta, new_ver, tmpdir):
     with open(os.path.join(YAML_PATH, old_meta)) as fp:
         in_yaml = fp.read()
 
@@ -31,14 +31,14 @@ def test_qt_main(old_meta, new_meta, new_ver, tmpdir):
         out_yaml = fp.read()
 
     run_test_migration(
-        m=VERSION_WITH_QTQTMAIN,
+        m=VERSION_WITH_JPEGTURBO,
         inp=in_yaml,
         output=out_yaml,
         kwargs={"new_version": new_ver},
         prb="Dependencies have been updated if changed",
         mr_out={
             "migrator_name": "Version",
-            "migrator_version": VERSION_WITH_QTQTMAIN.migrator_version,
+            "migrator_version": VERSION_WITH_JPEGTURBO.migrator_version,
             "version": new_ver,
         },
         tmpdir=tmpdir,
