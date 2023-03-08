01aba472ff2730c1d4a19cc65851f5c7c98d4d21
beckermr <becker.mr@gmail.com>
2023-03-08 05:53:49 -0600

    ENH shared the json paths

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,23 +2,26 @@ import os
 import json
 import pickle
 
-from conda_forge_tick.utils import LazyJson, dumps
+from conda_forge_tick.utils import LazyJson, dumps, get_sharded_path
 
 
 def test_lazy_json(tmpdir):
     f = os.path.join(tmpdir, "hi.json")
-    assert not os.path.exists(f)
+    fpth = get_sharded_path(f)
+    assert not os.path.exists(fpth)
     lj = LazyJson(f)
-    assert os.path.exists(lj.file_name)
-    with open(f) as ff:
+    assert not os.path.exists(lj.file_name)
+    assert os.path.exists(lj.sharded_path)
+    assert fpth == lj.sharded_path
+    with open(fpth) as ff:
         assert ff.read() == json.dumps({})
     lj["hi"] = "world"
     assert lj["hi"] == "world"
-    assert os.path.exists(lj.file_name)
-    with open(f) as ff:
+    assert os.path.exists(lj.sharded_path)
+    with open(fpth) as ff:
         assert ff.read() == dumps({"hi": "world"})
     lj.update({"hi": "globe"})
-    with open(f) as ff:
+    with open(fpth) as ff:
         assert ff.read() == dumps({"hi": "globe"})
     p = pickle.dumps(lj)
     lj2 = pickle.loads(p)
@@ -27,7 +30,7 @@ def test_lazy_json(tmpdir):
 
     with lj as attrs:
         attrs.setdefault("lst", []).append("universe")
-    with open(f) as ff:
+    with open(fpth) as ff:
         assert ff.read() == dumps({"hi": "globe", "lst": ["universe"]})
 
     with lj as attrs:
@@ -35,17 +38,33 @@ def test_lazy_json(tmpdir):
         with lj as attrs_again:
             attrs_again.setdefault("lst", []).append("universe")
             attrs.setdefault("lst", []).append("universe")
-    with open(f) as ff:
+    with open(fpth) as ff:
         assert ff.read() == dumps({"hi": "globe", "lst": ["universe"] * 4})
 
     with lj as attrs:
         with lj as attrs_again:
             attrs_again.setdefault("lst2", []).append("universe")
             attrs.setdefault("lst2", []).append("universe")
-    with open(f) as ff:
+    with open(fpth) as ff:
         assert ff.read() == dumps(
             {"hi": "globe", "lst": ["universe"] * 4, "lst2": ["universe"] * 2},
         )
+
+    with lj as attrs:
+        del attrs["lst"]
+    with open(fpth) as ff:
+        assert ff.read() == dumps(
+            {"hi": "globe", "lst2": ["universe"] * 2},
+        )
+
+    with lj as attrs:
+        attrs.pop("lst2")
+    with open(fpth) as ff:
+        assert ff.read() == dumps({"hi": "globe"})
+
+    assert len(lj) == 1
+    assert {k for k in lj} == {"hi"}
+
     lj.clear()
-    with open(f) as ff:
+    with open(fpth) as ff:
         assert ff.read() == dumps({})
