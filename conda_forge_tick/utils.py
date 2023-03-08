b479ddc16c4867f770a7d0876c2c3df89db06647
beckermr <becker.mr@gmail.com>
2023-03-08 05:30:04 -0600

    remove old stuff

--- conda_forge_tick/utils.py
+++ conda_forge_tick/utils.py
@@ -648,26 +648,6 @@ def dump_graph_json(gx: nx.DiGraph, filename: str = "graph.json") -> None:
         dump(nld, f)
 
 
-def dump_graph_dynamo(
-    gx: nx.DiGraph,
-    tablename: str = "graph",
-    region: str = "us-east-2",
-) -> None:
-    print(f"DynamoDB dump to {tablename} in {region}")
-    ddb = boto3.resource("dynamodb", region_name=region)
-    table = ddb.Table(tablename)
-    with table.batch_writer() as batch:
-        for node in gx.nodes:
-            if not node:
-                continue
-            preds = [n for n in gx.predecessors(node) if n]
-            preds.sort()
-            item = {"node_id": node}
-            if preds:
-                item["predecessors"] = preds
-            batch.put_item(Item=item)
-
-
 def dump_graph(
     gx: nx.DiGraph,
     filename: str = "graph.json",
