36e65e7e4ccd321899cecd2f3d11e276cec3802b
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-07-01 06:07:21 -0500

    BUG pushd hides exit status maybe (#1729)

--- autotick-bot/install_bot_code.sh
+++ autotick-bot/install_bot_code.sh
@@ -4,11 +4,12 @@ git config --global user.name regro-cf-autotick-bot
 git config --global user.email 36490558+regro-cf-autotick-bot@users.noreply.github.com
 git config --global pull.rebase false
 
-pushd cf-scripts
+cd cf-scripts
 
 export GIT_FULL_HASH=$(git rev-parse HEAD)
 pip install -e .
-popd
+
+cd ..
 
 git clone --depth=100 https://github.com/regro/cf-graph-countyfair.git cf-graph
 git clone --depth=1 https://github.com/conda-forge/conda-forge-pinning-feedstock.git
