32b9c6aae510380230a7c14a161f8e59b1603a27
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-07-11 17:57:23 -0500

    ENH update everything

--- autotick-bot/install_bot_code.sh
+++ autotick-bot/install_bot_code.sh
@@ -4,7 +4,7 @@ git config --global user.name regro-cf-autotick-bot
 git config --global user.email 36490558+regro-cf-autotick-bot@users.noreply.github.com
 git config --global pull.rebase false
 
-micromamba update conda-forge-pinning conda-smithy conda-build mamba
+micromamba update --all
 
 cd cf-scripts
 
