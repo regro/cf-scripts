78814000d4fb0d9eceebcccfe2a5681b3750be67
beckermr <becker.mr@gmail.com>
2023-08-08 06:28:09 -0500

    TST test one more thing

--- tests/test_upstream_versions.py
+++ tests/test_upstream_versions.py
@@ -4,7 +4,7 @@ from flaky import flaky
 
 from conda.models.version import VersionOrder
 from conda_forge_tick.update_upstream_versions import get_latest_version
-from conda_forge_tick.update_sources import NPM, next_version, RawURL, NVIDIA, Github
+from conda_forge_tick.update_sources import NPM, next_version, PyPI, RawURL, NVIDIA, Github
 
 from conda_forge_tick.utils import parse_meta_yaml
 from conda_forge_tick.lazy_json_backends import LazyJson
@@ -869,6 +869,7 @@ def test_latest_version_aws_sdk_cpp(tmpdir):
                 "meta_yaml": parse_meta_yaml(inp),
             },
         )
-    attempt = get_latest_version(name, pmy, [Github(), RawURL()])
+    attempt = get_latest_version(name, pmy, [PyPI(), Github(), RawURL()])
     assert attempt["new_version"] is not None
     assert attempt["new_version"]
+    print(attempt)
