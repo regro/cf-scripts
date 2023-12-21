fface1eda57c88f3edfa856041005575918b47e5
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-12-21 06:11:43 -0600

    BUG no update since it appears to downgrade

--- autotick-bot/install_bot_code.sh
+++ autotick-bot/install_bot_code.sh
@@ -4,9 +4,6 @@ git config --global user.name regro-cf-autotick-bot
 git config --global user.email 36490558+regro-cf-autotick-bot@users.noreply.github.com
 git config --global pull.rebase false
 
-conda update --all --yes
-conda update conda-forge-pinning --yes
-
 cd cf-scripts
 
 export GIT_FULL_HASH=$(git rev-parse HEAD)
