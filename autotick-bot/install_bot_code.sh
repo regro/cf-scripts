91485a0c0891fc29dcd39c942b3fe6e2faa003be
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-12-20 20:02:00 -0600

    Update install_bot_code.sh

--- autotick-bot/install_bot_code.sh
+++ autotick-bot/install_bot_code.sh
@@ -4,8 +4,6 @@ git config --global user.name regro-cf-autotick-bot
 git config --global user.email 36490558+regro-cf-autotick-bot@users.noreply.github.com
 git config --global pull.rebase false
 
-micromamba update --all
-
 cd cf-scripts
 
 export GIT_FULL_HASH=$(git rev-parse HEAD)
