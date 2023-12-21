22f72a82451d6bf09d2e4bb578dc2286bb877603
beckermr <becker.mr@gmail.com>
2023-12-21 05:11:21 -0600

    REF back to conda

--- autotick-bot/install_bot_code.sh
+++ autotick-bot/install_bot_code.sh
@@ -4,6 +4,8 @@ git config --global user.name regro-cf-autotick-bot
 git config --global user.email 36490558+regro-cf-autotick-bot@users.noreply.github.com
 git config --global pull.rebase false
 
+conda update --all --yes
+
 cd cf-scripts
 
 export GIT_FULL_HASH=$(git rev-parse HEAD)
