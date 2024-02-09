e11f2b9af134ab93f5b7157c2a83f7c8479da3fd
Yannik Tausch <yannik.tausch@quantco.com>
2024-02-09 11:48:09 +0100

    add mock CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,10 @@
+from unittest import mock
+
 import pytest
 from click.testing import CliRunner
 
 from conda_forge_tick.cli import main
+from conda_forge_tick.cli_context import CliContext
 
 commands = (
     "auto-tick",
@@ -64,3 +67,70 @@ def test_invalid_job(command: str, job: int, n_jobs: int):
     result = runner.invoke(main, [command, f"--job={job}", f"--n-jobs={n_jobs}"])
     assert result.exit_code == 2
     assert "in the range" in result.output
+
+
+takes_context_commands = (
+    "gather-all-feedstocks",
+    "make-graph",
+    "update-upstream-versions",
+    "auto-tick",
+    "update-prs",
+    "deploy-to-github",
+    "backup-lazy-json",
+    "sync-lazy-json-across-backends",
+    "cache-lazy-json-to-disk",
+)
+
+
+@pytest.mark.parametrize("debug", [True, False])
+@pytest.mark.parametrize("dry_run", [True, False])
+@pytest.mark.parametrize(
+    "command, patch_function",
+    [
+        ("gather-all-feedstocks", "conda_forge_tick.all_feedstocks.main"),
+        ("make-graph", "conda_forge_tick.make_graph.main"),
+        (
+            "update-upstream-versions",
+            "conda_forge_tick.update_upstream_versions.main",
+        ),
+        ("auto-tick", "conda_forge_tick.auto_tick.main"),
+        ("make-status-report", "conda_forge_tick.status_report.main"),
+        ("update-prs", "conda_forge_tick.update_prs.main"),
+        ("make-mappings", "conda_forge_tick.mappings.main"),
+        ("deploy-to-github", "conda_forge_tick.deploy.deploy"),
+        ("backup-lazy-json", "conda_forge_tick.lazy_json_backups.main_backup"),
+        (
+            "sync-lazy-json-across-backends",
+            "conda_forge_tick.lazy_json_backends.main_sync",
+        ),
+        (
+            "cache-lazy-json-to-disk",
+            "conda_forge_tick.lazy_json_backends.main_cache",
+        ),
+    ],
+)
+def test_cli_mock_commands_pass_context(
+    debug: bool,
+    dry_run: bool,
+    command: str,
+    patch_function: str,
+):
+    runner = CliRunner()
+    debug_flag = "--debug" if debug else "--no-debug"
+    dry_run_flag = "--dry-run" if dry_run else "--no-dry-run"
+
+    with mock.patch(patch_function) as cmd_mock:
+        result = runner.invoke(main, [debug_flag, dry_run_flag, command])
+        assert result.exit_code == 0
+        cmd_mock.assert_called_once()
+
+        if command not in takes_context_commands:
+            for arg in cmd_mock.call_args.args:
+                assert not isinstance(arg, CliContext)
+            for kwarg in cmd_mock.call_args.kwargs.values():
+                assert not isinstance(kwarg, CliContext)
+            return
+
+        command_context: CliContext = cmd_mock.call_args.args[0]
+        assert command_context.debug is debug
+        assert command_context.dry_run is dry_run
