9d99c0f438f4e8d946c7f9acfb6df61db16bedb5
Yannik Tausch <yannik.tausch@quantco.com>
2024-03-04 19:18:08 +0100

    continue specifying fields

--- conda_forge_tick/models/meta_yaml.py
+++ conda_forge_tick/models/meta_yaml.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
 
+from conda_forge_tick.models.common import StrictBaseModel
+
 """
 Refer to https://docs.conda.io/projects/conda-build/en/stable/resources/define-metadata.html for
 a documentation of the fields.
 """
 
 
+class Package(StrictBaseModel):
+    name: str
+    version: str
+
+
 class MetaYaml(BaseModel):
-    # TODO: define
-    pass
+    package: Package
