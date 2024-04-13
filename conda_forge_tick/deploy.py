14e789df976bbd2172a1122e4a6dfe7b33e3209b
beckermr <becker.mr@gmail.com>
2024-04-13 07:30:50 -0500

    ENH better deploy in batches

--- conda_forge_tick/deploy.py
+++ conda_forge_tick/deploy.py
@@ -80,91 +80,99 @@ def deploy(ctx: CliContext, dirs_to_deploy: list[str] = None):
             .splitlines(),
         )
 
+    print("found %d files to add" % len(files_to_add), flush=True)
+
     n_added = 0
-    for file in files_to_add:
-        if file and os.path.exists(file):
+    max_per_batch = 500
+    batch = 0
+    while files_to_add:
+        batch += 1
+        n_added_this_batch = 0
+        while files_to_add and n_added_this_batch < max_per_batch:
+            file = files_to_add.pop()
+            if file and os.path.exists(file):
+                try:
+                    print(f"committing {n_added: >5d}: {file}", flush=True)
+                    _run_git_cmd(f"git add {file}")
+                    n_added_this_batch += 1
+                    n_added += 1
+                except Exception as e:
+                    print(e)
+
+        if n_added_this_batch > 0:
             try:
-                print(f"committing: {file}", flush=True)
-                _run_git_cmd(f"git add {file}")
-                n_added += 1
+                _step_name = os.environ.get("GITHUB_WORKFLOW", "update graph")
+                _run_git_cmd(f'git commit -am "{_step_name} {batch: >3d} - {BUILD_URL}"')
             except Exception as e:
                 print(e)
 
-    if n_added > 0:
-        try:
-            _step_name = os.environ.get("GITHUB_WORKFLOW", "update graph")
-            _run_git_cmd(f'git commit -am "{_step_name} - {BUILD_URL}"')
-        except Exception as e:
-            print(e)
-
-        # make sure the graph can load, if not we will error
-        try:
-            gx = load_existing_graph()
-            # TODO: be more selective about which json to check
-            for node, attrs in gx.nodes.items():
-                attrs["payload"]._load()
-            graph_ok = True
-        except Exception:
-            graph_ok = False
-
-        status = 1
-        num_try = 0
-        while status != 0 and num_try < 10 and graph_ok:
+            # make sure the graph can load, if not we will error
             try:
-                if num_try == 9:
+                gx = load_existing_graph()
+                # TODO: be more selective about which json to check
+                for node, attrs in gx.nodes.items():
+                    attrs["payload"]._load()
+                graph_ok = True
+            except Exception:
+                graph_ok = False
+
+            status = 1
+            num_try = 0
+            while status != 0 and num_try < 10 and graph_ok:
+                try:
+                    if num_try == 9:
+                        print(
+                            "\n\n>>>>>>>>>>>> git unshallow for try %d\n\n" % num_try,
+                            flush=True,
+                        )
+                        _run_git_cmd("git fetch --unshallow")
+                    print("\n\n>>>>>>>>>>>> git pull try %d\n\n" % num_try, flush=True)
+                    _run_git_cmd("git pull -s recursive -X theirs")
+                except Exception as e:
                     print(
-                        "\n\n>>>>>>>>>>>> git unshallow for try %d\n\n" % num_try,
+                        "\n\n>>>>>>>>>>>> git pull try %d failed: %s \n\n" % (num_try, e),
                         flush=True,
                     )
-                    _run_git_cmd("git fetch --unshallow")
-                print("\n\n>>>>>>>>>>>> git pull try %d\n\n" % num_try, flush=True)
-                _run_git_cmd("git pull -s recursive -X theirs")
-            except Exception as e:
-                print(
-                    "\n\n>>>>>>>>>>>> git pull try %d failed: %s \n\n" % (num_try, e),
-                    flush=True,
-                )
-                pass
-            print("\n\n>>>>>>>>>>>> git push try %d\n\n" % num_try, flush=True)
-            with sensitive_env() as env:
-                status = doctr_run(
-                    [
-                        "git",
-                        "push",
-                        "https://{token}@github.com/{deploy_repo}.git".format(
-                            token=env.get("PASSWORD", ""),
-                            deploy_repo="regro/cf-graph-countyfair",
-                        ),
-                        "master",
-                    ],
-                    token=env.get("PASSWORD", "").encode("utf-8"),
-                )
-            num_try += 1
+                    pass
+                print("\n\n>>>>>>>>>>>> git push try %d\n\n" % num_try, flush=True)
+                with sensitive_env() as env:
+                    status = doctr_run(
+                        [
+                            "git",
+                            "push",
+                            "https://{token}@github.com/{deploy_repo}.git".format(
+                                token=env.get("PASSWORD", ""),
+                                deploy_repo="regro/cf-graph-countyfair",
+                            ),
+                            "master",
+                        ],
+                        token=env.get("PASSWORD", "").encode("utf-8"),
+                    )
+                num_try += 1
 
-        if status != 0 or not graph_ok:
-            print("\n\n>>>>>>>>>>>> failed deploy - pushing to branch\n\n", flush=True)
-            if not graph_ok:
-                print("\n\n>>>>>>>>>>>> git unshallow graph bad\n\n", flush=True)
-                _run_git_cmd("git fetch --unshallow")
-            _branch = "failed-circle-run-%s" % os.environ["CIRCLE_BUILD_NUM"]
-            _run_git_cmd(f"git checkout -b {_branch}")
-            _run_git_cmd("git commit --allow-empty -am 'help me!'")
+            if status != 0 or not graph_ok:
+                # this never works so we'll just stop
+                # print("\n\n>>>>>>>>>>>> failed deploy - pushing to branch\n\n", flush=True)
+                # if not graph_ok:
+                #     print("\n\n>>>>>>>>>>>> git unshallow graph bad\n\n", flush=True)
+                #     _run_git_cmd("git fetch --unshallow")
+                # _branch = "failed-circle-run-%s" % os.environ["CIRCLE_BUILD_NUM"]
+                # _run_git_cmd(f"git checkout -b {_branch}")
+                # _run_git_cmd("git commit --allow-empty -am 'help me!'")
 
-            with sensitive_env() as env:
-                status = doctr_run(
-                    [
-                        "git",
-                        "push",
-                        "--set-upstream",
-                        "https://{token}@github.com/{deploy_repo}.git".format(
-                            token=env.get("PASSWORD", ""),
-                            deploy_repo="regro/cf-graph-countyfair",
-                        ),
-                        _branch,
-                    ],
-                    token=env.get("PASSWORD", "").encode("utf-8"),
-                )
+                # with sensitive_env() as env:
+                #     status = doctr_run(
+                #         [
+                #             "git",
+                #             "push",
+                #             "--set-upstream",
+                #             "https://{token}@github.com/{deploy_repo}.git".format(
+                #                 token=env.get("PASSWORD", ""),
+                #                 deploy_repo="regro/cf-graph-countyfair",
+                #             ),
+                #             _branch,
+                #         ],
+                #         token=env.get("PASSWORD", "").encode("utf-8"),
+                #     )
 
-            raise RuntimeError("bot did not push its data! stopping!")
-    else:
-        print("no files to commit!", flush=True)
+                raise RuntimeError("bot did not push its data! stopping!")
