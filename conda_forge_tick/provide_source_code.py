1cb4321e2b0ffd6774b6077f9dd73d0c7a85d611
beckermr <becker.mr@gmail.com>
2024-04-21 06:13:48 -0500

    WIP provide source from container

---
+++ conda_forge_tick/provide_source_code.py
@@ -0,0 +1,114 @@
+import logging
+import os
+import subprocess
+import tempfile
+from contextlib import contextmanager
+
+from conda_forge_tick.os_utils import sync_dirs
+from conda_forge_tick.utils import run_container_task, CB_CONFIG
+
+logger = logging.getLogger(__name__)
+
+
+@contextmanager
+def provide_source_code(recipe_dir, use_container=True):
+    """Context manager to provide the source code for a recipe.
+
+    Parameters
+    ----------
+    recipe_dir : str
+        The path to the recipe directory.
+
+    Returns
+    -------
+    str
+        The path to the source code directory.
+    """
+
+    in_container = os.environ.get("CF_TICK_IN_CONTAINER", "false") == "true"
+    if use_container is None:
+        use_container = not in_container
+
+    if use_container and not in_container:
+        with provide_source_code_containerized(recipe_dir) as source_dir:
+            yield source_dir
+    else:
+        with provide_source_code_local(recipe_dir) as source_dir:
+            yield source_dir
+
+
+@contextmanager
+def provide_source_code_containerized(recipe_dir, use_container=True):
+    """Context manager to provide the source code for a recipe.
+
+    **This function runs recipe parsing in a container and then provides
+    the source code in a tmpdir on the host.**
+
+    Parameters
+    ----------
+    recipe_dir : str
+        The path to the recipe directory.
+
+    Returns
+    -------
+    str
+        The path to the source code directory.
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_recipe_dir = os.path.join(tmpdir, "recipe_dir")
+        sync_dirs(
+            recipe_dir, tmp_recipe_dir, ignore_dot_git=True, update_git=False
+        )
+
+        os.chmod(tmpdir, 0o777)
+        subprocess.run(["chmod", "-R", "777", tmpdir], check=True, capture_output=True)
+
+        logger.info(f"host recipe dir {recipe_dir}: {os.listdir(recipe_dir)}")
+        logger.info(
+            f"copied host recipe dir {tmp_recipe_dir}: {os.listdir(tmp_recipe_dir)}"
+        )
+
+        run_container_task(
+            "provide-source-code",
+            [],
+            mount_readonly=False,
+            mount_dir=tmpdir,
+        )
+
+        yield os.path.join(tmpdir, "source_dir")
+
+
+@contextmanager
+def provide_source_code_local(recipe_dir):
+    """Context manager to provide the source code for a recipe.
+
+    Parameters
+    ----------
+    recipe_dir : str
+        The path to the recipe directory.
+
+    Returns
+    -------
+    str
+        The path to the source code directory.
+    """
+    try:
+        from conda_build.api import render
+        from conda_build.config import Config
+        from conda_build.source import provide
+
+        # Use conda build to do all the downloading/extracting bits
+        md = render(
+            recipe_dir,
+            config=Config(**CB_CONFIG),
+            finalize=False,
+            bypass_env_check=True,
+        )
+        if not md:
+            return None
+        md = md[0][0]
+
+        # provide source dir
+        yield provide(md)
+    except (SystemExit, Exception) as e:
+        raise RuntimeError("conda build src exception:" + str(e))
