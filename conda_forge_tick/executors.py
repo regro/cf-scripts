3774f98b828b22bf09534549d4c7228cfe019700
Yannik Tausch <yannik.tausch@quantco.com>
2024-05-27 14:42:36 +0200

    typo

--- conda_forge_tick/executors.py
+++ conda_forge_tick/executors.py
@@ -72,7 +72,7 @@ def _init_process(lock):
 
 def _init_dask(lock):
     global DRLOCK
-    # it appears we have to construct the locak by name instead
+    # it appears we have to construct the lock by name instead
     # of passing the object itself
     # otherwise dask uses a regular lock
     DRLOCK = DaskRLock(name=lock)
