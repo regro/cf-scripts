8cbbd5934124d09cc8b4fd19db5e4eaf40ebdaec
Isuru Fernando <isuruf@gmail.com>
2024-06-27 18:22:49 -0500

    pre-commit

--- conda_forge_tick/migrators/__init__.py
+++ conda_forge_tick/migrators/__init__.py
@@ -29,7 +29,7 @@ from .numpy2 import Numpy2Migrator
 from .pip_check import PipCheckMigrator
 from .pip_wheel_dep import PipWheelMigrator
 from .qt_to_qt_main import QtQtMainMigrator
-from .replacement import Replacement
 from .r_ucrt import RUCRTCleanup
+from .replacement import Replacement
 from .use_pip import PipMigrator
 from .version import Version
--- conda_forge_tick/migrators/r_ucrt.py
+++ conda_forge_tick/migrators/r_ucrt.py
@@ -1,4 +1,3 @@
-import re
 import typing
 from typing import Any
 
