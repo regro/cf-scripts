0b3f203c8ee455e754093b94b2e04d59c5ecd0d9
Yannik Tausch <yannik.tausch@quantco.com>
2024-07-28 14:02:28 +0200

    use tmpdir for feedstocks instead of static directory first step of #2812

--- conda_forge_tick/contexts.py
+++ conda_forge_tick/contexts.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 import os
+import tempfile
 import typing
+from collections.abc import Iterator
+from contextlib import contextmanager
 from dataclasses import dataclass
+from pathlib import Path
 
 from networkx import DiGraph
 
@@ -27,10 +33,10 @@ class MigratorSessionContext:
     dry_run: bool = True
 
 
-@dataclass
+@dataclass(frozen=True)
 class FeedstockContext:
     feedstock_name: str
-    attrs: "AttrsTypedDict"
+    attrs: AttrsTypedDict
     _default_branch: str = None
 
     @property
@@ -40,6 +46,34 @@ class FeedstockContext:
         else:
             return self._default_branch
 
-    @default_branch.setter
-    def default_branch(self, v):
-        self._default_branch = v
+    @property
+    def git_repo_name(self) -> str:
+        return f"{self.feedstock_name}-feedstock"
+
+    @contextmanager
+    def reserve_clone_directory(self) -> Iterator[ClonedFeedstockContext]:
+        """
+        Reserve a temporary directory for the feedstock repository that will be available within the context manager.
+        The returned context object will contain the path to the feedstock repository in local_clone_dir.
+        After the context manager exits, the temporary directory will be deleted.
+        """
+        with tempfile.TemporaryDirectory() as tmpdir:
+            local_clone_dir = Path(tmpdir) / self.git_repo_name
+            local_clone_dir.mkdir()
+            yield ClonedFeedstockContext(
+                **self.__dict__,
+                local_clone_dir=local_clone_dir,
+            )
+
+
+@dataclass(frozen=True, kw_only=True)
+class ClonedFeedstockContext(FeedstockContext):
+    """
+    A FeedstockContext object that has reserved a temporary directory for the feedstock repository.
+
+    Implementation Note: Keep this class frozen or there will be consistency issues if someone modifies
+    a ClonedFeedstockContext object in place - it will not be reflected in the original FeedstockContext object.
+
+    """
+
+    local_clone_dir: Path
