9d9894f380b4c47e37eb863af129eacc102ac08d
Yannik Tausch <yannik.tausch@quantco.com>
2024-07-28 16:12:40 +0200

    refactor: replace get_repo with new git backend logic
    squash! refactor: replace get_repo with new git backend logic

--- conda_forge_tick/contexts.py
+++ conda_forge_tick/contexts.py
@@ -43,6 +43,10 @@ class FeedstockContext:
             return self._default_branch
 
     @property
+    def git_repo_owner(self) -> str:
+        return "conda-forge"
+
+    @property
     def git_repo_name(self) -> str:
         return f"{self.feedstock_name}-feedstock"
 
