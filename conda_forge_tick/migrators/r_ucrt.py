5180feb51f0556e13c20fb3feabf3f89587ceb99
nichmor <nmorkotilo@gmail.com>
2024-08-07 12:02:26 +0300

    misc: apply for other migrators

--- conda_forge_tick/migrators/r_ucrt.py
+++ conda_forge_tick/migrators/r_ucrt.py
@@ -1,5 +1,7 @@
+import os
 import re
 import typing
+import logging
 from typing import Any
 
 from conda_forge_tick.migrators.core import MiniMigrator
@@ -7,6 +9,8 @@ from conda_forge_tick.os_utils import pushd
 
 if typing.TYPE_CHECKING:
     from ..migrators_types import AttrsTypedDict
+
+logger = logging.getLogger(__name__)
 
 
 def _cleanup_raw_yaml(raw_yaml):
@@ -47,6 +51,9 @@ class RUCRTCleanup(MiniMigrator):
 
     def migrate(self, recipe_dir: str, attrs: "AttrsTypedDict", **kwargs: Any) -> None:
         with pushd(recipe_dir):
+            if not os.path.exists("meta.yaml") and os.path.exists("recipe.yaml"):
+                logger.info(f"Skipping {self.__class__.__name__} for recipe.yaml")
+                return
             with open("meta.yaml") as fp:
                 raw_yaml = fp.read()
 
--- conda_forge_tick/migrators/use_pip.py
+++ conda_forge_tick/migrators/use_pip.py
@@ -1,4 +1,6 @@
+import os
 import typing
+import logging
 from typing import Any
 
 from conda_forge_tick.os_utils import pushd
@@ -8,6 +10,8 @@ from .core import MiniMigrator
 
 if typing.TYPE_CHECKING:
     from ..migrators_types import AttrsTypedDict
+
+logger = logging.getLogger(__name__)
 
 
 class PipMigrator(MiniMigrator):
@@ -24,6 +28,9 @@ class PipMigrator(MiniMigrator):
 
     def migrate(self, recipe_dir: str, attrs: "AttrsTypedDict", **kwargs: Any) -> None:
         with pushd(recipe_dir):
+            if not os.path.exists("meta.yaml") and os.path.exists("recipe.yaml"):
+                logger.info(f"Skipping {self.__class__.__name__} for recipe.yaml")
+                return
             with open("meta.yaml") as fp:
                 lines = fp.readlines()
 
