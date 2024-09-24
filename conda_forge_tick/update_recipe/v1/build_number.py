8ca6b11499cc5b4333d704e8c9604662f9694f3c
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-09-24 17:05:16 +0200

    allow callable

--- conda_forge_tick/update_recipe/v1/build_number.py
+++ conda_forge_tick/update_recipe/v1/build_number.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING, Any, Literal
+import re
+from typing import TYPE_CHECKING, Any, Callable, Literal
 
 from conda_forge_tick.update_recipe.v1.yaml import _dump_yaml_to_str, _load_yaml
 
@@ -11,6 +12,26 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 HashType = Literal["md5", "sha256"]
+
+RE_PATTERN = re.compile(r"(?:build|build_number|number):\s*(\d+)")
+
+
+def old_build_number(recipe_text: str) -> int:
+    """
+    Extract the build number from the recipe text.
+
+    Arguments:
+    ----------
+    * `recipe_text` - The recipe text.
+
+    Returns:
+    --------
+    * The build number.
+    """
+    match = re.search(RE_PATTERN, recipe_text)
+    if match is not None:
+        return int(match.group(1))
+    return 0
 
 
 def _update_build_number_in_context(
@@ -40,7 +61,7 @@ def _update_build_number_in_recipe(
     return is_modified
 
 
-def update_build_number(file: Path, new_build_number: int = 0) -> str:
+def update_build_number(file: Path, new_build_number: int | Callable = 0) -> str:
     """
     Update the build number in the recipe file.
 
@@ -54,7 +75,13 @@ def update_build_number(file: Path, new_build_number: int = 0) -> str:
     * The updated recipe as a string.
     """
     data = _load_yaml(file)
+
+    if callable(new_build_number):
+        detected_build_number = old_build_number(file.read_text())
+        new_build_number = new_build_number(detected_build_number)
+
     build_number_modified = _update_build_number_in_context(data, new_build_number)
+
     if not build_number_modified:
         _update_build_number_in_recipe(data, new_build_number)
 
