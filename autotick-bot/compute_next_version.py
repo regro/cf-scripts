302a73cc795bda3fe6a99dfdb29168dac7631160
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-10-23 07:17:57 -0500

    Update compute_next_version.py

--- autotick-bot/compute_next_version.py
+++ autotick-bot/compute_next_version.py
@@ -1,22 +1,29 @@
 import datetime
 import subprocess
+import sys
 
 # get the current date for tagging below
 now = datetime.datetime.utcnow()
 
 # get the most recent tag
 res = subprocess.run(
-    ["git", "describe", "--tags", "--abbrev=0"],
+    ["git", "--no-pager", "tag", "--sort=committerdate"],
     capture_output=True,
     text=True,
 )
 
-if res.returncode != 0:
+if res.returncode != 0 or (not res.stdout.strip()):
     # no tags so compute the first version
     new_version = f"{now.year}.{now.month}.0"
 else:
     # we have a tag so bump
-    curr_version = res.stdout.strip()
+    curr_version = None
+    for line in res.stdout.splitlines():
+        line = line.strip()
+        if line:
+            curr_version = line
+    assert curr_version is not None
+    print("found current version: %s" % line, file=sys.stderr, flush=True)
 
     # figure out if we bump the major, minor or patch version
     major_minor, patch = curr_version.rsplit(".", 1)
