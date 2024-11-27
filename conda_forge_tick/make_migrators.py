99895cfab7862b403d3ef5793ff3ca41e38ba249
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-11-27 11:28:21 -0600

    Update conda_forge_tick/make_migrators.py

--- conda_forge_tick/make_migrators.py
+++ conda_forge_tick/make_migrators.py
@@ -768,6 +768,7 @@ def initialize_migrators(
         gx,
         cast("PackageName", "astropy"),
         cast("PackageName", "astropy-base"),
+        "The astropy feedstock has been split into two packages, astropy-base only "
         "has required dependencies and astropy now has all optional dependencies. "
         "To maintain the old behavior you should migrate to astropy-base.",
     )
