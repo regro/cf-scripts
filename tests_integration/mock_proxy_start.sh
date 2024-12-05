9e15297b0812f5c66a889cb9f173b9b93386ccce
Yannik Tausch <yannik.tausch@quantco.com>
2024-12-05 20:11:13 +0100

    debug

--- tests_integration/mock_proxy_start.sh
+++ tests_integration/mock_proxy_start.sh
@@ -3,4 +3,4 @@
 set -euxo pipefail
 
 # You might need to set PYTHONPATH to the root of cf-scripts
-mitmdump -s ./tests_integration/mock_server_addon.py --set connection_strategy=lazy --set upstream_cert=false
+mitmdump -s ./tests_integration/mock_server_addon.py --set connection_strategy=lazy --set upstream_cert=false | tee /tmp/mitmproxy.log
