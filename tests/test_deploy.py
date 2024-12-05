2b5074a8dd3598430dfc46b09a8b28129abaf70d
beckermr <becker.mr@gmail.com>
2024-12-04 23:36:39 -0600

    test: missed the file

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,28 +1,28 @@
 import os
 import tempfile
 
-from conda_forge_tick.os_utils import pushd
-from conda_forge_tick.deploy import _parse_gh_conflicts
-
 import pytest
 
+from conda_forge_tick.deploy import _parse_gh_conflicts
+from conda_forge_tick.os_utils import pushd
 
-@pytest.mark.parametrize("output,fnames", [
-    (
-        "",
-        []
-    ),
-    (
-        """\
+
+@pytest.mark.parametrize(
+    "output,fnames",
+    [
+        ("", []),
+        (
+            """\
 error: The following untracked working tree files would be overwritten by merge:
 	pr_info/8/1/0/1/f/devtools.json
 Please move or remove them before you merge.
 Aborting
 Merge with strategy recursive failed.
 """,
-        ["pr_info/8/1/0/1/f/devtools.json"]
-    ),
-])
+            ["pr_info/8/1/0/1/f/devtools.json"],
+        ),
+    ],
+)
 def test_deploy_parse_gh_conflicts(output, fnames):
     with tempfile.TemporaryDirectory() as tmpdir, pushd(str(tmpdir)):
         for fname in fnames:
