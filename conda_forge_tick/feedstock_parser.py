43e69edc748f62b6203fe6a17c7e378cfd0270dd
Yannik Tausch <yannik.tausch@quantco.com>
2025-01-14 10:45:34 +0100

    fix: use CONDA_FORGE_ORG

--- conda_forge_tick/feedstock_parser.py
+++ conda_forge_tick/feedstock_parser.py
@@ -192,12 +192,11 @@ def _extract_requirements(meta_yaml, outputs_to_keep=None):
 
 
 def _fetch_static_repo(name, dest):
-    conda_forge_org = os.getenv(ENV_OVERRIDE_CONDA_FORGE_ORG, "conda-forge")
     found_branch = None
     for branch in ["main", "master"]:
         try:
             r = requests.get(
-                f"https://github.com/{conda_forge_org}/{name}-feedstock/archive/{branch}.zip",
+                f"https://github.com/{CONDA_FORGE_ORG}/{name}-feedstock/archive/{branch}.zip",
             )
             r.raise_for_status()
             found_branch = branch
