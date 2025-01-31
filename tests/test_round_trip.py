51d4116a713f89a70c0f6d7bd263c9921966459d
beckermr <becker.mr@gmail.com>
2025-01-31 05:08:27 -0600

    fix: address code review

--- tests/test_round_trip.py
+++ tests/test_round_trip.py
@@ -1,24 +1,28 @@
 import tempfile
+import textwrap
 
 from conda_forge_tick.migrators.round_trip import YAMLRoundTrip
 
 
 def test_round_trip_v0():
-    myaml = """\
-blah: &foo # [unix]
-- blah  # [osx]
-- foo
-foo: *foo
-"""
-
-    fmyaml = """\
-blah:   # [unix]
-  - blah # [osx]
-  - foo
-foo:
-  - blah # [osx]
-  - foo
-"""
+    myaml = textwrap.dedent(
+        """\
+        blah: &foo # [unix]
+        - blah  # [osx]
+        - foo
+        foo: *foo
+        """
+    )
+    fmyaml = textwrap.dedent(
+        """\
+        blah:   # [unix]
+          - blah # [osx]
+          - foo
+        foo:
+          - blah # [osx]
+          - foo
+        """
+    )
 
     with tempfile.TemporaryDirectory() as d:
         with open(f"{d}/meta.yaml", "w") as f:
@@ -27,19 +31,24 @@ foo:
         with open(f"{d}/meta.yaml") as f:
             assert f.read() == fmyaml
 
+    assert not YAMLRoundTrip().filter({"meta_yaml": {"schema_version": 0}})
+
 
 def test_round_trip_v1():
-    myaml = """\
-blah:
-- blah # a dep
-- foo
-"""
-
-    fmyaml = """\
-blah:
-  - blah # a dep
-  - foo
-"""
+    myaml = textwrap.dedent(
+        """\
+        blah:
+        - blah # a dep
+        - foo
+        """
+    )
+    fmyaml = textwrap.dedent(
+        """\
+        blah:
+          - blah # a dep
+          - foo
+        """
+    )
 
     with tempfile.TemporaryDirectory() as d:
         with open(f"{d}/recipe.yaml", "w") as f:
@@ -47,3 +56,5 @@ blah:
         YAMLRoundTrip().migrate(d, {})
         with open(f"{d}/recipe.yaml") as f:
             assert f.read() == fmyaml
+
+    assert not YAMLRoundTrip().filter({"meta_yaml": {"schema_version": 1}})
