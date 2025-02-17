a93d81c3c950709714df7518003df198ad29e2ea
beckermr <becker.mr@gmail.com>
2025-02-17 08:32:38 -0600

    test: allow for xz extension in flaky test

--- tests/test_qt_qt_main.py
+++ tests/test_qt_qt_main.py
@@ -19,7 +19,7 @@ YAML_PATH = os.path.join(os.path.dirname(__file__), "test_yaml")
     [
         (
             "qtqtmain_octave_before_meta.yaml",
-            "qtqtmain_octave_after_meta.yaml",
+            ("qtqtmain_octave_after_meta.yaml", "qtqtmain_octave_xz_after_meta.yaml"),
             "7.1.0",
         ),
         ("qtqtmain_qgis_before_meta.yaml", "qtqtmain_qgis_after_meta.yaml", "3.18.3"),
@@ -29,20 +29,43 @@ def test_qt_main(old_meta, new_meta, new_ver, tmpdir):
     with open(os.path.join(YAML_PATH, old_meta)) as fp:
         in_yaml = fp.read()
 
-    with open(os.path.join(YAML_PATH, new_meta)) as fp:
-        out_yaml = fp.read()
+    if isinstance(new_meta, tuple):
+        out_yamls = []
 
-    run_test_migration(
-        m=VERSION_WITH_QTQTMAIN,
-        inp=in_yaml,
-        output=out_yaml,
-        kwargs={"new_version": new_ver},
-        prb="Dependencies have been updated if changed",
-        mr_out={
-            "migrator_name": Version.name,
-            "migrator_version": VERSION_WITH_QTQTMAIN.migrator_version,
-            "version": new_ver,
-        },
-        tmpdir=tmpdir,
-        should_filter=False,
-    )
+        for nm in new_meta:
+            with open(os.path.join(YAML_PATH, nm)) as fp:
+                out_yamls.append(fp.read())
+    else:
+        with open(os.path.join(YAML_PATH, new_meta)) as fp:
+            out_yamls = [fp.read()]
+
+    failed = []
+    excepts = []
+    for out_yaml in out_yamls:
+        try:
+            run_test_migration(
+                m=VERSION_WITH_QTQTMAIN,
+                inp=in_yaml,
+                output=out_yaml,
+                kwargs={"new_version": new_ver},
+                prb="Dependencies have been updated if changed",
+                mr_out={
+                    "migrator_name": Version.name,
+                    "migrator_version": VERSION_WITH_QTQTMAIN.migrator_version,
+                    "version": new_ver,
+                },
+                tmpdir=tmpdir,
+                should_filter=False,
+            )
+        except Exception as e:
+            failed.append(True)
+            excepts.append(e)
+        else:
+            failed.append(False)
+            excepts.append(None)
+
+    for e in excepts:
+        if e is not None:
+            raise e
+
+    assert not all(failed)
