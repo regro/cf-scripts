0cb073df421143819263311eee749ef1ce0f752a
beckermr <becker.mr@gmail.com>
2025-02-25 15:40:33 -0600

    feat: finished utility functions and tests

--- conda_forge_tick/migrators/staticlib.py
+++ conda_forge_tick/migrators/staticlib.py
@@ -20,6 +20,7 @@ from conda_forge_tick.migrators.core import (
 )
 from conda_forge_tick.os_utils import pushd
 from conda_forge_tick.utils import (
+    extract_section_from_yaml_text,
     get_keys_default,
     get_migrator_name,
 )
@@ -31,59 +32,203 @@ RNG = secrets.SystemRandom()
 logger = logging.getLogger(__name__)
 
 
+def _left_gt_right_dist(ld, rd):
+    lver = VersionOrder(ld.version)
+    lbuild = ld.build_number
+
+    rver = VersionOrder(rd.version)
+    rbuild = rd.build_number
+
+    if (lver > rver) or (lver == rver and lbuild > rbuild):
+        return True
+    else:
+        return False
+
+
+@lru_cache(maxsize=128)
+def _cached_dist_from_str(req: str) -> Dist:
+    return Dist.from_string(req, channel_override="conda-forge")
+
+
+@lru_cache(maxsize=128)
+def _cached_match_spec(req: str) -> MatchSpec:
+    return MatchSpec(req)
+
+
+@lru_cache(maxsize=128)
+def _match_spec_is_exact(ms: MatchSpec) -> bool:
+    if (
+        ms.get_exact_value("name") is not None
+        and ms.get_exact_value("version") is not None
+        and ms.get_exact_value("build") is not None
+    ):
+        return True
+    else:
+        return False
+
+
+@lru_cache(maxsize=128)
+def _match_spec_to_dist(ms: MatchSpec) -> Dist:
+    if _match_spec_is_exact(ms):
+        mstr = ms.conda_build_form().replace(" ", "-")
+        return _cached_dist_from_str(mstr)
+    else:
+        raise ValueError("MatchSpec is not exact: %s" % ms.conda_build_form())
+
+
 @lru_cache(maxsize=128)
 def get_latest_static_lib(host_req: str, platform_arch: str) -> Dist:
     platform_arch = platform_arch.replace("_", "-")
     rd_fn = fetch_repodata([platform_arch])[0]
     with open(rd_fn) as fp:
         rd = rapidjson.load(fp)
-    ms = MatchSpec(host_req)
+    ms = _cached_match_spec(host_req)
 
-    max_ver = None
-    max_build = None
     max_dist = None
     for key in ["packages", "packages.conda"]:
-        for on in rd[key]:
-            if on.rsplit("-", 2)[0] == ms.name:
-                dist = Dist.from_string(on, channel_override="conda-forge")
+        for fn in rd[key]:
+            if fn.rsplit("-", 2)[0] == ms.name:
+                dist = _cached_dist_from_str(fn)
                 if ms.match(dist):
-                    ver = VersionOrder(dist.version)
-                    build = dist.build_number
-
-                    if max_ver is None:
-                        max_ver = ver
-                        max_build = build
+                    if max_dist is None:
                         max_dist = dist
-                    elif (ver > max_ver) or (ver == max_ver and build > max_build):
-                        max_ver = ver
-                        max_build = build
-                        max_dist = dist
+                    else:
+                        if _left_gt_right_dist(dist, max_dist):
+                            max_dist = dist
 
     if max_dist is None:
         raise ValueError(
-            f"Could not find a static lib for {host_req} on {platform_arch}!"
+            f"Could not find a static lib for `{host_req}` on `{platform_arch}`!"
         )
     return max_dist
 
 
+@lru_cache(maxsize=128)
+def platform_has_dist(platform_arch: str, dist: Dist) -> bool:
+    platform_arch = platform_arch.replace("_", "-")
+    rd_fn = fetch_repodata([platform_arch])[0]
+    with open(rd_fn) as fp:
+        rd = rapidjson.load(fp)
+
+    fn_dist = "-".join(dist.quad[:3])
+
+    for key in ["packages", "packages.conda"]:
+        for fn in rd[key]:
+            if fn_dist in fn:
+                return True
+    return False
+
+
+def extract_static_libs_from_meta_yaml_text(
+    meta_yaml_texts: tuple[str],
+    static_linking_host_requirement: str,
+    platform_arch: str | None = None,
+) -> set[Dist]:
+    ms = _cached_match_spec(static_linking_host_requirement)
+
+    dists = set()
+    for my in meta_yaml_texts:
+        for line in my.splitlines():
+            line = line.strip()
+            if line.startswith("-"):
+                line = line[1:].strip()
+                line = line.split("#", maxsplit=1)[0].strip()
+                try:
+                    rms = _cached_match_spec(line)
+                except Exception:
+                    logger.debug(
+                        "could not parse line: %s into a MatchSpec. It will be ignored!",
+                        line,
+                    )
+                    continue
+                if _match_spec_is_exact(rms) and rms.get_exact_value(
+                    "name"
+                ) == ms.get_exact_value("name"):
+                    dist = _match_spec_to_dist(rms)
+                    if platform_arch is None or platform_has_dist(platform_arch, dist):
+                        dists.add(dist)
+
+    return dists
+
+
+@lru_cache(maxsize=128)
 def any_static_libs_out_of_date(
-    static_linking_host_requirements: list[str],
+    static_linking_host_requirements: tuple[str],
+    platform_arches: tuple[str],
     raw_meta_yaml: str,
-    platform_arches: list[str],
-) -> bool:
+) -> (bool, dict[str, dict[str, str]]):
     # for each plat-arch combo, find latest static lib version
     # and compare to meta.yaml
     # if any of them are out of date, return True
     # else return False
+    # also require that all latest static libs
+    # have the same version and build number
+    host_sections = extract_section_from_yaml_text(raw_meta_yaml, "host")
 
+    static_lib_replacements = {}
     for platform_arch in platform_arches:
-        for slhr in static_linking_host_requirements:
-            ld = get_latest_static_lib(slhr, platform_arch)
-            ldstr = ld.to_match_spec().conda_build_form()
-            if ldstr not in raw_meta_yaml:
-                return True
+        static_lib_replacements[platform_arch] = {}
 
-    return False
+    out_of_date = False
+    for slhr in static_linking_host_requirements:
+        curr_ver_build = (None, None)
+        for platform_arch in platform_arches:
+            ld = get_latest_static_lib(slhr, platform_arch)
+
+            # if we find different versions, we bail since there
+            # could be a race condition or something else going on
+            if curr_ver_build == (None, None):
+                curr_ver_build = (ld.version, ld.build_number)
+            elif curr_ver_build != (ld.version, ld.build_number):
+                return False, static_lib_replacements
+
+            ldstr = ld.to_match_spec().conda_build_form()
+
+            # test if version in meta_yaml is less than version we found
+            # if so, set out_of_date = True
+            # always add static lib from recipe to old_static_libs
+            _old_dists = extract_static_libs_from_meta_yaml_text(
+                host_sections,
+                slhr,
+                platform_arch,
+            )
+            for _old_dist in _old_dists:
+                # add to set of replacements if needs update
+                if _left_gt_right_dist(ld, _old_dist):
+                    out_of_date = True
+                    odstr = _old_dist.to_match_spec().conda_build_form()
+                    static_lib_replacements[platform_arch][odstr] = ldstr
+
+    return out_of_date, static_lib_replacements
+
+
+def attempt_update_static_libs(
+    raw_meta_yaml: str,
+    static_lib_replacements: dict[str, dict[str, str]],
+) -> (bool, str):
+    """Attempt to update static lib versions in meta.yaml.
+
+    Returns True if the recipe was updated, False otherwise.
+    Also returns the new recipe.
+    """
+    updated = False
+
+    for plat_arch in static_lib_replacements:
+        for old_spec, new_spec in static_lib_replacements[plat_arch].items():
+            new_raw_meta_yaml = raw_meta_yaml.replace(
+                old_spec,
+                new_spec,
+            )
+            if new_raw_meta_yaml != raw_meta_yaml:
+                updated = True
+                raw_meta_yaml = new_raw_meta_yaml
+                logger.debug(
+                    "static lib '%s' updated to '%s'",
+                    old_spec,
+                    new_spec,
+                )
+
+    return updated, raw_meta_yaml
 
 
 class StaticLibMigrator(GraphMigrator):
@@ -158,11 +303,12 @@ class StaticLibMigrator(GraphMigrator):
             if len(sl_host_req) == 0:
                 continue
 
+            platform_arches = tuple(payload.get("platforms", []) or [])
             if any_static_libs_out_of_date(
-                static_linking_host_requirements=sl_host_req,
+                static_linking_host_requirements=tuple(sl_host_req),
+                platform_arches=platform_arches,
                 raw_meta_yaml=payload.get("raw_meta_yaml", "") or "",
-                platform_arches=payload.get("platforms", []) or [],
-            ):
+            )[0]:
                 logger.debug("not yet built for new static libs: %s" % node)
                 return True
 
@@ -190,11 +336,12 @@ class StaticLibMigrator(GraphMigrator):
         if not has_static_libs:
             return True
 
+        platform_arches = tuple(attrs.get("platforms", []) or [])
         static_libs_out_of_date = any_static_libs_out_of_date(
-            static_linking_host_requirements=sl_host_req,
+            static_linking_host_requirements=tuple(sl_host_req),
+            platform_arches=platform_arches,
             raw_meta_yaml=attrs.get("raw_meta_yaml", "") or "",
-            platform_arches=attrs.get("platforms", []) or [],
-        )
+        )[0]
 
         return (
             (not has_static_libs)
@@ -208,17 +355,58 @@ class StaticLibMigrator(GraphMigrator):
     def migrate(
         self, recipe_dir: str, attrs: "AttrsTypedDict", **kwargs: Any
     ) -> "MigrationUidTypedDict":
-        # TODO
         # for each plat-arch combo, find latest static lib version
         # and update the meta.yaml if needed
+        sl_host_req = get_keys_default(
+            attrs,
+            ["meta_yaml", "extra", "static_linking_host_requirements"],
+            dict(),
+            list(),
+        )
+        platform_arches = tuple(attrs.get("platforms", []) or [])
 
         with pushd(recipe_dir):
             if os.path.exists("recipe.yaml"):
-                self.set_build_number("recipe.yaml")
+                with open("recipe.yaml") as f:
+                    raw_meta_yaml = f.read()
             else:
-                self.set_build_number("meta.yaml")
+                with open("meta.yaml") as f:
+                    raw_meta_yaml = f.read()
 
-        return super().migrate(recipe_dir, attrs)
+        needs_update, static_lib_replacements = any_static_libs_out_of_date(
+            static_linking_host_requirements=tuple(sl_host_req),
+            platform_arches=platform_arches,
+            raw_meta_yaml=raw_meta_yaml,
+        )
+
+        if needs_update:
+            updated_recipe, new_raw_meta_yaml = attempt_update_static_libs(
+                raw_meta_yaml=raw_meta_yaml,
+                static_lib_replacements=static_lib_replacements,
+            )
+
+            if updated_recipe:
+                with pushd(recipe_dir):
+                    if os.path.exists("recipe.yaml"):
+                        with open("recipe.yaml", "w") as f:
+                            f.write(new_raw_meta_yaml)
+                        self.set_build_number("recipe.yaml")
+                    else:
+                        with open("meta.yaml", "w") as f:
+                            f.write(new_raw_meta_yaml)
+                        self.set_build_number("meta.yaml")
+            else:
+                needs_update = False
+                logger.debug(
+                    "static libs not updated for feedstock '%s' for host requirements '%s'",
+                    attrs.get("feedstock_name", "!!NONAME!!"),
+                    sl_host_req,
+                )
+        muid = super().migrate(recipe_dir, attrs)
+        if not needs_update:
+            muid["already_done"] = True
+
+        return muid
 
     def pr_body(self, feedstock_ctx: ClonedFeedstockContext) -> str:
         body = super().pr_body(feedstock_ctx)
@@ -257,4 +445,26 @@ class StaticLibMigrator(GraphMigrator):
     def migrator_uid(self, attrs: "AttrsTypedDict") -> "MigrationUidTypedDict":
         n = super().migrator_uid(attrs)
         n["name"] = self.name
+
+        # build a unique string from the latest static libs found
+        # prevents duplicate PRs or missed PRs
+        # get_latest_static_libs caches the results so the return value
+        # is stable for a given execution of the bot
+        platform_arches = tuple(attrs.get("platforms", []) or [])
+        sl_host_req = get_keys_default(
+            attrs,
+            ["meta_yaml", "extra", "static_linking_host_requirements"],
+            dict(),
+            list(),
+        )
+        vals = []
+        for slhr in sl_host_req:
+            for platform_arch in platform_arches:
+                ld = get_latest_static_lib(slhr, platform_arch)
+                ldstr = ld.to_match_spec().conda_build_form()
+                ldstr = "::".join([platform_arch.replace("_", "-"), ldstr])
+                vals.append(ldstr)
+        ustr = ";".join(sorted(vals))
+        n["static_libs"] = ustr
+
         return n
---
+++ tests/test_staticlib_migrator.py
@@ -0,0 +1,275 @@
+import textwrap
+
+import pytest
+
+from conda_forge_tick.migrators.staticlib import (
+    _cached_dist_from_str,
+    _cached_match_spec,
+    _left_gt_right_dist,
+    _match_spec_is_exact,
+    _match_spec_to_dist,
+    any_static_libs_out_of_date,
+    extract_static_libs_from_meta_yaml_text,
+    get_latest_static_lib,
+)
+
+
+@pytest.mark.parametrize(
+    "ld,rd,res",
+    [
+        (
+            _cached_dist_from_str("libfoo-0.9.0-h1234_0"),
+            _cached_dist_from_str("libfoo-1.0.0-h1234_0"),
+            False,
+        ),
+        (
+            _cached_dist_from_str("libfoo-1.0.0-h1234_0"),
+            _cached_dist_from_str("libfoo-1.0.0-h1234_0"),
+            False,
+        ),
+        (
+            _cached_dist_from_str("libfoo-1.0.0-h1234_0"),
+            _cached_dist_from_str("libfoo-1.0.0-h1234_1"),
+            False,
+        ),
+        (
+            _cached_dist_from_str("libfoo-1.0.0-h1234_1"),
+            _cached_dist_from_str("libfoo-1.0.0-h1234_0"),
+            True,
+        ),
+        (
+            _cached_dist_from_str("libfoo-1.0.1-h1234_0"),
+            _cached_dist_from_str("libfoo-1.0.0-h1234_0"),
+            True,
+        ),
+        (
+            _cached_dist_from_str("libfoo-1.0.1-h1234_0"),
+            _cached_dist_from_str("libfoo-1.0.0-h1234_1"),
+            True,
+        ),
+    ],
+)
+def test_left_gt_right_dist(ld, rd, res):
+    assert _left_gt_right_dist(ld, rd) is res
+    if ld != rd:
+        assert _left_gt_right_dist(rd, ld) is (not res)
+
+
+@pytest.mark.parametrize(
+    "mstr,res",
+    [
+        ("libfoo * *", False),
+        ("libfoo >=10", False),
+        ("libfoo", False),
+        ("libfoo ==10 *_6", False),
+        ("libfoo 10 h67_5", True),
+        ("libfoo 10.* h67_5", False),
+    ],
+)
+def test_match_spec_is_exact(mstr, res):
+    assert _match_spec_is_exact(_cached_match_spec(mstr)) is res
+
+
+def test_match_spec_to_dist_works():
+    mstr = _cached_match_spec("libfoo 10 h67_5")
+    dist = _match_spec_to_dist(mstr)
+    assert dist.name == mstr.get_exact_value("name")
+    assert dist.version == mstr.get_exact_value("version")
+    assert dist.build == mstr.get_exact_value("build")
+
+
+def test_match_spec_to_dist_raises():
+    with pytest.raises(ValueError):
+        _match_spec_to_dist(_cached_match_spec("libfoo >=10"))
+
+
+def test_get_latest_static_lib():
+    ld = get_latest_static_lib("llvmdev", "osx-64")
+    ld15 = get_latest_static_lib("llvmdev 15.*", "osx-64")
+    assert ld.version.split(".")[0] > ld15.version.split(".")[0]
+
+    ld15b4 = get_latest_static_lib("llvmdev 15.* *_4", "osx-64")
+    ld15b5 = get_latest_static_lib("llvmdev 15.* *_5", "osx-64")
+    assert ld15b4.version.split(".")[0] == ld15b5.version.split(".")[0]
+    assert ld15b4.build_number < ld15b5.build_number
+    assert ld15b4.build_number == 4
+    assert ld15b5.build_number == 5
+
+    with pytest.raises(ValueError):
+        get_latest_static_lib("llvmdev 15.* *_100000", "osx-64")
+
+
+@pytest.mark.parametrize(
+    "meta_yaml_text,slhr,expected",
+    [
+        (
+            (
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10 h67_5
+                      - libfoo 10.*
+                      # libfood 10 h2t_4
+                      {% random jinja %}
+
+                      - libbar 10 h2t_4
+                    """
+                )[1:-1],
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10.*
+                      - libbar 10 h2t_4
+                    """
+                )[1:-1],
+            ),
+            "libfoo 10.*",
+            {"libfoo 10 h67_5"},
+        ),
+        (
+            (
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10 h67_5
+                      - libfoo 10.*
+                      - libfoo 10 h2t_4
+                    """
+                )[1:-1],
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10.*
+                      - libbar 10 h2t_4
+                    """
+                )[1:-1],
+            ),
+            "libfoo 10.*",
+            {"libfoo 10 h67_5", "libfoo 10 h2t_4"},
+        ),
+        (
+            (
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10 h67_5
+                      - libfoo 10.*
+                      - libbar 10 h2t_4
+                    """
+                )[1:-1],
+            ),
+            "libfoo 10.*",
+            {"libfoo 10 h67_5"},
+        ),
+        (
+            (
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10 h67_5
+                      - libfoo 10.*
+                      - libbar 10 h2t_4
+                    """
+                )[1:-1],
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10.*
+                      - libbar 10 h2t_4
+                    """
+                )[1:-1],
+            ),
+            "libf 10.*",
+            set(),
+        ),
+        (
+            (
+                textwrap.dedent(
+                    """
+                    host:
+                      - libfoo 10 h67_5
+                      - libfoo 10.*
+                      - libbar 10 h2t_4
+                    """
+                )[1:-1],
+            ),
+            "libf 10.*",
+            set(),
+        ),
+    ],
+)
+def test_extract_static_libs_from_meta_yaml_text(meta_yaml_text, slhr, expected):
+    static_libs = extract_static_libs_from_meta_yaml_text(meta_yaml_text, slhr)
+    static_libs = {sl.to_matchspec() for sl in static_libs}
+    assert static_libs == expected
+
+
+@pytest.mark.parametrize(
+    "recipe,slhr,expected_ood,expected_slrep",
+    [
+        (
+            textwrap.dedent(
+                """
+                requirements:
+                  host:
+                    - blah
+                """
+            )[1:-1],
+            ("llvm 15.*",),
+            False,
+            {"osx-64": {}, "osx-arm64": {}},
+        ),
+        (
+            textwrap.dedent(
+                """
+                requirements:
+                  host:
+                    - {}
+                    - llvm 15.*
+                """.format(get_latest_static_lib("llvm 15.*", "osx-64").to_matchspec())
+            )[1:-1],
+            ("llvm 15.*",),
+            False,
+            {"osx-64": {}, "osx-arm64": {}},
+        ),
+        (
+            textwrap.dedent(
+                """
+                requirements:
+                  host:
+                    - {}
+                    - {}
+                """.format(
+                    get_latest_static_lib("llvm 13.*", "osx-64").to_matchspec(),
+                    get_latest_static_lib("llvm 13.*", "osx-arm64").to_matchspec(),
+                )
+            )[1:-1],
+            ("llvm 14.*",),
+            True,
+            {
+                "osx-64": {
+                    get_latest_static_lib(
+                        "llvm 13.*", "osx-64"
+                    ).to_matchspec(): get_latest_static_lib(
+                        "llvm 14.*", "osx-64"
+                    ).to_matchspec()
+                },
+                "osx-arm64": {
+                    get_latest_static_lib(
+                        "llvm 13.*", "osx-arm64"
+                    ).to_matchspec(): get_latest_static_lib(
+                        "llvm 14.*", "osx-arm64"
+                    ).to_matchspec()
+                },
+            },
+        ),
+    ],
+)
+def test_any_static_libs_out_of_date(recipe, slhr, expected_ood, expected_slrep):
+    ood, slrep = any_static_libs_out_of_date(
+        static_linking_host_requirements=slhr,
+        platform_arches=("osx-64", "osx-arm64"),
+        raw_meta_yaml=recipe,
+    )
+    assert ood == expected_ood
+    assert slrep == expected_slrep
