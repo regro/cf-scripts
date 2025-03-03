630a34ee093f50b0ed99578d4321d0d1a05b5810
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-03-03 11:42:05 -0600

    Update _parser.py

--- conda_forge_tick/recipe_parser/_parser.py
+++ conda_forge_tick/recipe_parser/_parser.py
@@ -54,7 +54,7 @@ def _get_yaml_parser(typ="jinja2"):
     def represent_none(self, data):
         return self.represent_scalar("tag:yaml.org,2002:null", "")
 
-    parser = YAML(typo=typo)
+    parser = YAML(typ=typ)
     parser.indent(mapping=2, sequence=4, offset=2)
     parser.width = 320
     parser.preserve_quotes = True
