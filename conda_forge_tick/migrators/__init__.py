478ead1b5b85edfc2c8a90e2a68ad1e11a3c0ec6
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-03-13 04:25:04 -0500

    Revert "Replace `XzLibLzmaDevelMigrator` with `MiniReplacement`"

--- conda_forge_tick/migrators/__init__.py
+++ conda_forge_tick/migrators/__init__.py
@@ -26,6 +26,7 @@ from .duplicate_lines import DuplicateLinesCleanup
 from .extra_jinj2a_keys_cleanup import ExtraJinja2KeysCleanup
 from .flang import FlangMigrator
 from .jinja2_vars_cleanup import Jinja2VarsCleanup
+from .jpegturbo import JpegTurboMigrator
 from .libboost import LibboostMigrator
 from .license import LicenseMigrator
 from .matplotlib_base import MatplotlibBase
@@ -35,10 +36,12 @@ from .numpy2 import Numpy2Migrator
 from .pip_check import PipCheckMigrator
 from .pip_wheel_dep import PipWheelMigrator
 from .pypi_org import PyPIOrgMigrator
+from .qt_to_qt_main import QtQtMainMigrator
 from .r_ucrt import RUCRTCleanup
-from .replacement import Replacement, MiniReplacement
+from .replacement import Replacement
 from .use_pip import PipMigrator
 from .version import Version
+from .xz_to_liblzma_devel import XzLibLzmaDevelMigrator
 from .noarch_python_min import NoarchPythonMinMigrator
 from .round_trip import YAMLRoundTrip
 from .staticlib import StaticLibMigrator
