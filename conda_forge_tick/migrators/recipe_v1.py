b8f69ae42512814d271b35b42d1dee705c30cdd4
Michał Górny <mgorny@gentoo.org>
2025-03-16 16:19:46 +0100

    Use a breadth-first replacement algorithm
    Thanks to @beckermr for the suggestion and the boilerplate.

--- conda_forge_tick/migrators/recipe_v1.py
+++ conda_forge_tick/migrators/recipe_v1.py
@@ -1,10 +1,10 @@
-import dataclasses
 import logging
 import typing
 from pathlib import Path
-from typing import Any, Generator
+from typing import Any
 
 from conda_forge_tick.migrators.core import MiniMigrator
+from conda_forge_tick.recipe_parser._parser import _get_yaml_parser
 
 if typing.TYPE_CHECKING:
     from ..migrators_types import AttrsTypedDict
@@ -12,33 +12,67 @@ if typing.TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-@dataclasses.dataclass
-class ConditionClause:
-    # "then" / "else"
-    name: str
-    # if True, we are currently reading inside the clause
-    collecting: bool = False
-    # if True, at least the heading was printed already
-    printed: bool = False
-    # remaining items (not printed yet)
-    items: list[str] = dataclasses.field(default_factory=list)
+def combine_conditions(node):
+    """Breadth first recursive call to combine list conditions"""
 
+    # recursion is breadth first because we go through each element here
+    # before calling `combine_conditions` on any element in the node
+    if isinstance(node, list):
+        # 1. loop through list elements, gather the if conditions
 
-@dataclasses.dataclass
-class ConditionState:
-    # complete line with the condition
-    condition: str
-    # base indent for the condition (i.e. up to "- if")
-    indent: str
-    # either "then" or "else" if it was printed last, or None if nothing was printed yet
-    last_clause_printed: str | None = None
-    # the data for then/else clauses
-    thens: ConditionClause = dataclasses.field(
-        default_factory=lambda: ConditionClause("then")
-    )
-    elses: ConditionClause = dataclasses.field(
-        default_factory=lambda: ConditionClause("else")
-    )
+        # condition ("if:") -> [(then, else), (then, else)...]
+        conditions = {}
+
+        for i in node:
+            if isinstance(i, dict) and "if" in i:
+                conditions.setdefault(i["if"], []).append((i["then"], i.get("else")))
+
+        # 2. if elements share a compatible if condition
+        # combine their if...then...else statements
+        to_drop = []
+        for i in range(len(node)):
+            if isinstance(node[i], dict) and "if" in node[i]:
+                condition = node[i]["if"]
+                if condition not in conditions:
+                    # already combined it, so drop the repeat instance
+                    to_drop.append(i)
+                    continue
+                if len(conditions[condition]) > 1:
+                    new_then = []
+                    new_else = []
+                    for sub_then, sub_else in conditions[node[i]["if"]]:
+                        if isinstance(sub_then, list):
+                            new_then.extend(sub_then)
+                        else:
+                            assert sub_then is not None
+                            new_then.append(sub_then)
+                        if isinstance(sub_else, list):
+                            new_else.extend(sub_else)
+                        elif sub_else is not None:
+                            new_else.append(sub_else)
+                    node[i]["then"] = new_then
+                    if new_else:
+                        # TODO: preserve inline "else" instead of converting it to a list?
+                        node[i]["else"] = new_else
+                    else:
+                        assert "else" not in node[i]
+                    # remove it from the dict, so we don't output it again
+                    del conditions[condition]
+
+        # drop the repeated conditions
+        for i in reversed(to_drop):
+            del node[i]
+
+    # then we descend down the tree
+    if isinstance(node, dict):
+        for k, v in node.items():
+            if k != "if":  # don't combine if statements
+                node[k] = combine_conditions(v)
+    elif isinstance(node, list):
+        for i in range(len(node)):
+            node[i] = combine_conditions(node[i])
+
+    return node
 
 
 class CombineV1ConditionsMigrator(MiniMigrator):
@@ -47,113 +81,11 @@ class CombineV1ConditionsMigrator(MiniMigrator):
 
     def migrate(self, recipe_dir: str, attrs: "AttrsTypedDict", **kwargs: Any) -> None:
         recipe_path = Path(recipe_dir) / "recipe.yaml"
-        recipe = recipe_path.read_text().splitlines()
+        parser = _get_yaml_parser(typ="rt")
+        with recipe_path.open() as f:
+            yaml = parser.load(f)
 
-        def flush_clause(
-            clause: ConditionClause, condition: ConditionState
-        ) -> Generator[str, None, None]:
-            """Print pending items from the clause"""
-            if clause.items:
-                if not clause.printed and len(clause.items) == 1:
-                    yield f"{condition.indent}{clause.name}: {clause.items[0]}\n"
-                else:
-                    if not clause.printed:
-                        yield f"{condition.indent}{clause.name}:\n"
-                    for subline in clause.items:
-                        yield f"{condition.indent}  - {subline}\n"
-                clause.printed = True
-                condition.last_clause_printed = clause.name
-            clause.items.clear()
+        yaml = combine_conditions(yaml)
 
-        def process_clause(
-            line: str,
-            clause: ConditionClause,
-            other: ConditionClause,
-            condition: ConditionState,
-        ) -> Generator[str, None, None]:
-            """Process the line starting a clause"""
-            if other.printed:
-                yield from flush_clause(other, condition)
-            clause.collecting = True
-            other.collecting = False
-            after = line.split(f"{clause.name}:", 1)[1].strip()
-            if after:
-                clause.items.append(after)
-            else:
-                clause.printed = True
-                condition.last_clause_printed = clause.name
-                yield f"{line}\n"
-
-        def process(recipe: list[str]) -> Generator[str, None, None]:
-            # stack of nested conditions
-            conditions: list[ConditionState] = []
-
-            for line_no, line in enumerate(recipe):
-                if conditions:
-                    condition = conditions[-1]
-                    if line.startswith(condition.indent):
-                        if line.lstrip().startswith("- if:"):
-                            # a nested condition
-                            indent = line.split("-")[0] + "  "
-                            conditions.append(ConditionState(line, indent))
-                            yield f"{line}\n"
-                        elif line.lstrip().startswith("then:"):
-                            yield from process_clause(
-                                line, condition.thens, condition.elses, condition
-                            )
-                        elif line.lstrip().startswith("else:"):
-                            yield from process_clause(
-                                line, condition.elses, condition.thens, condition
-                            )
-                        elif line.lstrip().startswith("-"):
-                            assert not (
-                                condition.thens.collecting
-                                and condition.elses.collecting
-                            )
-                            if condition.thens.collecting:
-                                condition.thens.items.append(
-                                    line.split("-", 1)[1].strip()
-                                )
-                            elif condition.elses.collecting:
-                                condition.elses.items.append(
-                                    line.split("-", 1)[1].strip()
-                                )
-                            else:
-                                raise RuntimeError(
-                                    f"then/else expected on line {line_no + 1}"
-                                )
-                        else:
-                            raise RuntimeError(
-                                f"then/else/list item expected on {line_no + 1}"
-                            )
-                        continue
-
-                    # we have left the previous condition
-                    # if we haven't printed "else" yet, we can combine with the next one
-                    if (
-                        line.startswith(condition.condition)
-                        and condition.last_clause_printed != "else"
-                    ):
-                        conditions[-1].thens.collecting = False
-                        conditions[-1].elses.collecting = False
-                        continue
-
-                    # prefer then/else order, unless we printed "else" already
-                    clause_order = [condition.thens, condition.elses]
-                    if condition.elses.printed:
-                        clause_order.reverse()
-                    for clause in clause_order:
-                        yield from flush_clause(clause, condition)
-                    conditions.pop()
-
-                # a top-level condition
-                if line.lstrip().startswith("- if:"):
-                    indent = line.split("-")[0] + "  "
-                    conditions.append(ConditionState(line, indent))
-                    yield f"{line}\n"
-                    continue
-
-                # lines outside a condition
-                yield f"{line}\n"
-
-        recipe_path.write_text("".join(process(recipe)))
+        with recipe_path.open("w") as f:
+            parser.dump(yaml, f)
