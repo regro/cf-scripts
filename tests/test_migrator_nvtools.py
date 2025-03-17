33a92faaf9b3c5b7dbea6b2c2ad9d5db8eb2e080
Daniel Ching <dching@nvidia.com>
2025-03-17 15:11:42 -0500

    TST: Restore nvidia migrator test files upon success

--- tests/test_migrator_nvtools.py
+++ tests/test_migrator_nvtools.py
@@ -18,24 +18,48 @@ mock_recipe_dir_ref = os.path.join(
 mock_node_attrs = {}
 
 
+def store_file_contents(filename):
+    with open(filename) as file:
+        return file.read()
+
+
+def write_file_contents(filename, buffer):
+    with open(filename, "w") as file:
+        file.write(buffer)
+
+
 def test_nvtools_migrate():
+    backups = [
+        store_file_contents(os.path.join(mock_recipe_dir, f))
+        for f in ["build.sh", "meta.yaml", "../conda-forge.yml"]
+    ]
+
     migrator = AddNVIDIATools()
     migrator.migrate(
         mock_recipe_dir,
         mock_node_attrs,
     )
-    assert filecmp.cmp(
-        os.path.join(mock_recipe_dir, "build.sh"),
-        os.path.join(mock_recipe_dir_ref, "build.sh"),
-        shallow=False,
+    success = (
+        filecmp.cmp(
+            os.path.join(mock_recipe_dir, "build.sh"),
+            os.path.join(mock_recipe_dir_ref, "build.sh"),
+            shallow=False,
+        )
+        and filecmp.cmp(
+            os.path.join(mock_recipe_dir, "meta.yaml"),
+            os.path.join(mock_recipe_dir_ref, "meta.yaml"),
+            shallow=False,
+        )
+        and filecmp.cmp(
+            os.path.join(mock_recipe_dir, "..", "conda-forge.yml"),
+            os.path.join(mock_recipe_dir_ref, "..", "conda-forge.yml"),
+            shallow=False,
+        )
     )
-    assert filecmp.cmp(
-        os.path.join(mock_recipe_dir, "meta.yaml"),
-        os.path.join(mock_recipe_dir_ref, "meta.yaml"),
-        shallow=False,
-    )
-    assert filecmp.cmp(
-        os.path.join(mock_recipe_dir, "..", "conda-forge.yml"),
-        os.path.join(mock_recipe_dir_ref, "..", "conda-forge.yml"),
-        shallow=False,
-    )
+    if success:
+        # Files only restored on success, so that you can see why the comparison failed
+        [
+            write_file_contents(os.path.join(mock_recipe_dir, f), b)
+            for f, b in zip(["build.sh", "meta.yaml", "../conda-forge.yml"], backups)
+        ]
+    assert success
