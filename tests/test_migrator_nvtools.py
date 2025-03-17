95718517b4f02bb8a3d0596b691e5afd89e478bb
Daniel Ching <dching@nvidia.com>
2025-03-17 11:37:46 -0500

    TST: Add reference recipe for nvtools migrator

--- tests/test_migrator_nvtools.py
+++ tests/test_migrator_nvtools.py
@@ -1,3 +1,4 @@
+import filecmp
 import os.path
 
 from conda_forge_tick.migrators import AddNVIDIATools
@@ -5,6 +6,12 @@ from conda_forge_tick.migrators import AddNVIDIATools
 mock_recipe_dir = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     "mock_nvtools_migrator_feedstock",
+    "recipe",
+)
+
+mock_recipe_dir_ref = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "mock_nvtools_migrator_feedstock_ref",
     "recipe",
 )
 
@@ -16,4 +23,19 @@ def test_nvtools_migrate():
     migrator.migrate(
         mock_recipe_dir,
         mock_node_attrs,
+    )
+    assert filecmp.cmp(
+        os.path.join(mock_recipe_dir, "build.sh"),
+        os.path.join(mock_recipe_dir_ref, "build.sh"),
+        shallow=False,
+    )
+    assert filecmp.cmp(
+        os.path.join(mock_recipe_dir, "meta.yaml"),
+        os.path.join(mock_recipe_dir_ref, "meta.yaml"),
+        shallow=False,
+    )
+    assert filecmp.cmp(
+        os.path.join(mock_recipe_dir, "..", "conda-forge.yml"),
+        os.path.join(mock_recipe_dir_ref, "..", "conda-forge.yml"),
+        shallow=False,
     )
