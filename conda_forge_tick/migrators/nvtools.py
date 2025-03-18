189df8938feb7db1b01c6c0de13a57b1ffd9c2f3
beckermr <becker.mr@gmail.com>
2025-03-18 15:47:13 -0500

    fix: migrator needs fixing too

--- conda_forge_tick/migrators/nvtools.py
+++ conda_forge_tick/migrators/nvtools.py
@@ -135,10 +135,22 @@ class AddNVIDIATools(Migrator):
         bool :
             True if node is to be skipped
         """
+        has_nvidia = False
+        if "meta_yaml" in attrs and "source" in attrs["meta_yaml"]:
+            if isinstance(attrs["meta_yaml"]["source"], list):
+                src_list = attrs["meta_yaml"]["source"]
+            else:
+                src_list = [attrs["meta_yaml"]["source"]]
+            for src in src_list:
+                src_url = src.get("url", "") or ""
+                has_nvidia = has_nvidia or (
+                    "https://developer.download.nvidia.com" in src_url
+                )
+
         return (
             super().filter(attrs)
             or attrs["archived"]
-            or "https://developer.download.nvidia.com" not in attrs["source"]["url"]
+            or (not has_nvidia)
         )
 
     def migrate(
