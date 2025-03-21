286b751b2880049023025559fe4501ee07cd6ad5
Yannik Tausch <yannik.tausch@quantco.com>
2025-03-21 18:29:18 +0100

    fix: remove side effects from load_feedstock and populate_feedstock_attributes

---
+++ tests/test_make_graph.py
@@ -0,0 +1,50 @@
+import pytest
+
+from conda_forge_tick.lazy_json_backends import LazyJson
+from conda_forge_tick.make_graph import try_load_feedstock
+from tests.conftest import FakeLazyJson
+
+
+@pytest.mark.parametrize("container_enabled", [True, False])
+@pytest.mark.parametrize("mark_not_archived", [True, False])
+def test_try_load_feedstock(
+    request: pytest.FixtureRequest, mark_not_archived: bool, container_enabled: bool
+):
+    """
+    If executed without containers (which is the default), try_load_feedstock should return the
+    correct LazyJSON data.
+    """
+    if container_enabled:
+        request.getfixturevalue("use_containers")
+
+    feedstock = "typst-test"  # archived
+
+    fake_lazy_json = FakeLazyJson()  # empty dict
+
+    with fake_lazy_json as loaded_lazy_json:
+        # noinspection PyTypeChecker
+        data = try_load_feedstock(feedstock, loaded_lazy_json, mark_not_archived).data  # type: ignore
+
+    if mark_not_archived:
+        assert data["archived"] is False
+    else:
+        # apparently, this is how the function behaves
+        assert "archived" not in data
+
+    assert data["feedstock_name"] == feedstock
+    assert data["parsing_error"] is False
+    assert data["raw_meta_yaml"].startswith("{% set name")
+    assert isinstance(data["conda-forge.yml"], dict)
+    assert "linux_64" in data["platforms"]
+    assert data["meta_yaml"]["about"]["license"] == "MIT"
+    assert isinstance(data["linux_64_meta_yaml"], dict)
+    assert isinstance(data["linux_64_requirements"], dict)
+    assert isinstance(data["total_requirements"], dict)
+    assert data["strong_exports"] is False
+    assert data["outputs_names"] == {feedstock}
+    assert isinstance(data["req"], set)
+    assert data["name"] == feedstock
+    assert data["version"].startswith("0.")
+    assert data["url"].startswith("https://github.com/tingerrr/typst-test")
+    assert data["hash_type"] == "sha256"
+    assert isinstance(data["version_pr_info"], LazyJson)
