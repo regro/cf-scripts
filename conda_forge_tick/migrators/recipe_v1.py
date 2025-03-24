f16a414012aee80f98b329e39b7e4649d731a275
Michał Górny <mgorny@gentoo.org>
2025-03-24 16:54:54 +0100

    Do not combine non-adjacent conditions in v1 recipes
    Modify the v1 recipe condition combiner not to merge non-adjacent
    conditions.  While this is not really a problem for dependencies (though
    it could break original dependency grouping), it did lead to reordering
    test commands that could lead to breaking the recipe.
    
    The algorithm aims to be as simple as possible.  It iterates over lists
    tail-to-head and folds an "if" into the preceding item if it has
    the same condition.
    
    Fixes #3933

--- conda_forge_tick/migrators/recipe_v1.py
+++ conda_forge_tick/migrators/recipe_v1.py
@@ -6,62 +6,51 @@ from typing import Any
 from conda_forge_tick.migrators.core import MiniMigrator
 from conda_forge_tick.recipe_parser._parser import _get_yaml_parser
 
+from typing import Any
+
 if typing.TYPE_CHECKING:
     from ..migrators_types import AttrsTypedDict
 
 logger = logging.getLogger(__name__)
 
 
-def combine_conditions(node):
+def is_same_condition(a: Any, b: Any) -> bool:
+    return (
+        isinstance(a, dict)
+        and isinstance(b, dict)
+        and "if" in a
+        and "if" in b
+        and a["if"] == b["if"]
+    )
+
+
+def fold_branch(source: Any, dest: Any, branch: str) -> None:
+    if branch not in source:
+        return
+    source_l = source[branch]
+    if isinstance(source_l, str):
+        source_l = [source_l]
+
+    if branch not in dest:
+        dest[branch] = []
+    elif isinstance(dest[branch], str):
+        dest[branch] = [dest[branch]]
+    dest[branch].extend(source_l)
+
+
+def combine_conditions(node: Any):
     """Breadth first recursive call to combine list conditions"""
 
     # recursion is breadth first because we go through each element here
     # before calling `combine_conditions` on any element in the node
     if isinstance(node, list):
-        # 1. loop through list elements, gather the if conditions
-
-        # condition ("if:") -> [(then, else), (then, else)...]
-        conditions = {}
-
-        for i in node:
-            if isinstance(i, dict) and "if" in i:
-                conditions.setdefault(i["if"], []).append((i["then"], i.get("else")))
-
-        # 2. if elements share a compatible if condition
-        # combine their if...then...else statements
-        to_drop = []
-        for i in range(len(node)):
-            if isinstance(node[i], dict) and "if" in node[i]:
-                condition = node[i]["if"]
-                if condition not in conditions:
-                    # already combined it, so drop the repeat instance
-                    to_drop.append(i)
-                    continue
-                if len(conditions[condition]) > 1:
-                    new_then = []
-                    new_else = []
-                    for sub_then, sub_else in conditions[node[i]["if"]]:
-                        if isinstance(sub_then, list):
-                            new_then.extend(sub_then)
-                        else:
-                            assert sub_then is not None
-                            new_then.append(sub_then)
-                        if isinstance(sub_else, list):
-                            new_else.extend(sub_else)
-                        elif sub_else is not None:
-                            new_else.append(sub_else)
-                    node[i]["then"] = new_then
-                    if new_else:
-                        # TODO: preserve inline "else" instead of converting it to a list?
-                        node[i]["else"] = new_else
-                    else:
-                        assert "else" not in node[i]
-                    # remove it from the dict, so we don't output it again
-                    del conditions[condition]
-
-        # drop the repeated conditions
-        for i in reversed(to_drop):
-            del node[i]
+        # iterate in reverse order, so we can remove elements on the fly
+        # start at index 1, since we can only fold to the previous node
+        for i in reversed(range(1, len(node))):
+            if is_same_condition(node[i], node[i - 1]):
+                fold_branch(node[i], node[i - 1], "then")
+                fold_branch(node[i], node[i - 1], "else")
+                del node[i]
 
     # then we descend down the tree
     if isinstance(node, dict):
