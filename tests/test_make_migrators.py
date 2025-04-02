40223ad012e98fb6364b4617fe9220d3bf043a01
Yannik Tausch <yannik.tausch@quantco.com>
2025-04-02 13:06:07 +0200

    feat: make create_migration_yaml_creator v1-compatible

--- tests/test_make_migrators.py
+++ tests/test_make_migrators.py
@@ -3,6 +3,7 @@ import shutil
 import tempfile
 from pathlib import Path
 
+import networkx as nx
 import pytest
 
 from conda_forge_tick.lazy_json_backends import get_sharded_path
@@ -34,27 +35,37 @@ class TestCreateMigrationYamlCreator:
             os.environ["CONDA_PREFIX"] = old_conda_prefix
 
     @pytest.fixture
-    def prepared_graph(self):
+    def prepared_graph(self, request: pytest.FixtureRequest):
+        """
+        Pass the node attrs files that should be present in the graph as
+        indirect fixture parameters.
+        """
+        node_attrs_files: list[str] = request.param
+
         with tempfile.TemporaryDirectory(prefix="cf-graph") as s_tmpdir:
             cf_graph_dir = Path(s_tmpdir)
             shutil.copy(TEST_GRAPH_FILE, cf_graph_dir / "graph.json")
 
-            numpy_dest = cf_graph_dir / get_sharded_path("node_attrs/numpy.json")
-            os.makedirs(numpy_dest.parent, exist_ok=True)
-            shutil.copy(NUMPY_NODE_ATTRS_FILE, numpy_dest)
-
-            pinning_dest = cf_graph_dir / get_sharded_path(
-                "node_attrs/conda-forge-pinning.json"
-            )
-            os.makedirs(pinning_dest.parent, exist_ok=True)
-            shutil.copy(CONDA_FORGE_PINNINGS_ATTRS_FILE, pinning_dest)
+            for node_attrs_file in node_attrs_files:
+                origin = TEST_FILES_DIR / f"{node_attrs_file}_node_attrs.json"
+                dest = cf_graph_dir / get_sharded_path(
+                    f"node_attrs/{node_attrs_file}.json"
+                )
+                os.makedirs(dest.parent, exist_ok=True)
+                shutil.copy(origin, dest)
 
             old_cwd = os.getcwd()
             os.chdir(cf_graph_dir)
             yield load_existing_graph()
             os.chdir(old_cwd)
 
-    def test_successful_recipe_v0(self, prepared_graph, inject_conda_build_config):
+    @pytest.mark.parametrize(
+        "prepared_graph", [["conda-forge-pinning", "numpy"]], indirect=True
+    )
+    def test_successful_recipe_v0(
+        self, prepared_graph: nx.DiGraph, inject_conda_build_config
+    ):
+        # feedstock under test: numpy
         migrators: list[MigrationYamlCreator] = []
         create_migration_yaml_creator(migrators, prepared_graph)
 
@@ -66,6 +77,28 @@ class TestCreateMigrationYamlCreator:
         assert migrator.current_pin == "1.26"
         assert migrator.new_pin_version == "2"
         assert migrator.pin_spec == "x"
+
+        assert len(migrator.effective_graph) == 1
+        assert "conda-forge-pinning" in migrator.effective_graph
+
+    @pytest.mark.parametrize(
+        "prepared_graph", [["conda-forge-pinning", "aws-c-io"]], indirect=True
+    )
+    def test_successful_recipe_v1(
+        self, prepared_graph: nx.DiGraph, inject_conda_build_config
+    ):
+        # feedstock under test: aws-c-io
+        migrators: list[MigrationYamlCreator] = []
+        create_migration_yaml_creator(migrators, prepared_graph)
+
+        assert len(migrators) == 1
+        migrator = migrators[0]
+
+        assert migrator.feedstock_name == "aws-c-io"
+        assert migrator.package_name == "aws_c_io"
+        assert migrator.current_pin == "0.15.3"
+        assert migrator.new_pin_version == "0.18.0"
+        assert migrator.pin_spec == "x.x.x"
 
         assert len(migrator.effective_graph) == 1
         assert "conda-forge-pinning" in migrator.effective_graph
