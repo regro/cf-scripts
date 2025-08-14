3e6443126498ef678e61e1314d3e20f28f961372
Jan Jagusch <jan.jagusch@gmail.com>
2025-08-14 15:39:25 +0200

    Add v1 test case for fastapi.

--- tests_integration/lib/_definitions/fastapi/__init__.py
+++ tests_integration/lib/_definitions/fastapi/__init__.py
@@ -9,29 +9,26 @@ class VersionUpdate(TestCase):
     def get_router(self) -> APIRouter:
         router = APIRouter()
 
-        @router.get("/pypi.org/pypi/pydantic/json")
+        @router.get("/pypi.org/pypi/fastapi/json")
         def handle_pypi_json_api():
             return {
                 # rest omitted
-                "info": {"name": "pydantic", "version": "2.10.2"}
+                "info": {"name": "fastapi", "version": "0.116.1"}
             }
 
         return router
 
     def prepare(self, helper: AbstractIntegrationTestHelper):
         feedstock_dir = Path(__file__).parent / "resources" / "feedstock"
-        helper.overwrite_feedstock_contents("pydantic", feedstock_dir)
-
-        feedstock_v1_dir = Path(__file__).parent / "resources" / "feedstock_v1"
-        helper.overwrite_feedstock_contents("pydantic", feedstock_v1_dir, branch="1.x")
+        helper.overwrite_feedstock_contents("fastapi", feedstock_dir)
 
     def validate(self, helper: AbstractIntegrationTestHelper):
         helper.assert_version_pr_present_v1(
-            "pydantic",
-            new_version="2.10.2",
-            new_hash="2bc2d7f17232e0841cbba4641e65ba1eb6fafb3a08de3a091ff3ce14a197c4fa",
-            old_version="2.10.1",
-            old_hash="a4daca2dc0aa429555e0656d6bf94873a7dc5f54ee42b1f5873d666fb3f35560",
+            "fastapi",
+            new_version="0.116.1",
+            new_hash="ed52cbf946abfd70c5a0dccb24673f0670deeb517a88b3544d03c2a6bf283143",
+            old_version="0.116.0",
+            old_hash="80dc0794627af0390353a6d1171618276616310d37d24faba6648398e57d687a",
         )
 
 
