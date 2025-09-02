38367a97338e107ea3c1acfb87e3bb7110fb52f9
Yannik Tausch <yannik.tausch@quantco.com>
2025-09-02 21:27:01 +0200

    fix

--- conda_forge_tick/feedstock_parser.py
+++ conda_forge_tick/feedstock_parser.py
@@ -30,11 +30,6 @@ if typing.TYPE_CHECKING:
 
     from conda_forge_tick.migrators_types import RecipeTypedDict
 
-from .migrators_types import PackageName, RequirementsTypedDict
-
-if typing.TYPE_CHECKING:
-    from mypy_extensions import TestTypedDict
-
 from conda_forge_tick.lazy_json_backends import LazyJson, dumps, loads
 from conda_forge_tick.utils import (
     as_iterable,
@@ -42,6 +37,8 @@ from conda_forge_tick.utils import (
     parse_recipe_yaml,
     sanitize_string,
 )
+
+from .migrators_types import PackageName, RequirementsTypedDict
 
 logger = logging.getLogger(__name__)
 
