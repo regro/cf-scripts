3006573fe043f1f405427b81149c6638d69cedd5
beckermr <becker.mr@gmail.com>
2025-09-19 14:40:33 -0500

    test: now returns none

--- tests/test_upstream_versions.py
+++ tests/test_upstream_versions.py
@@ -429,7 +429,7 @@ def test_latest_version_rawurl(name, inp, curr_ver, ver, source, urls, tmpdir):
 
 
 def test_latest_version_ca_policy_lcg(capfd, caplog):
-    assert get_latest_version("ca-policy-lcg", {}, [RawURL()]) == {"new_version": False}
+    assert get_latest_version("ca-policy-lcg", {}, [RawURL()]) == {"new_version": None}
     out, err = capfd.readouterr()
     all_output = out + err
     for record in caplog.records:
@@ -587,7 +587,7 @@ def test_latest_version_is_version_ignored(caplog):
     assert "Using URL https://source-a.com" in caplog.text
     assert "Ignoring version 1.2.3" in caplog.text
 
-    assert result == {"new_version": False}
+    assert result == {"new_version": None}
 
 
 def test_latest_version_no_sources_are_skipped(caplog):
