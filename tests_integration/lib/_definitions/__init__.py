03aad1dd6e79b283f7292f9246c69eed9c97135a
Yannik Tausch <yannik.tausch@quantco.com>
2025-12-04 10:51:39 +0000

    test: Add integration test for zizmor version update (#5144)
    * add zizmor feedstock submodule
    
    * test: Add integration test for zizmor version update
    
    * always print proxy logs
    
    * fix proxy
    
    ---------
    
    Co-authored-by: Matthew R. Becker <beckermr@users.noreply.github.com>

--- tests_integration/lib/_definitions/__init__.py
+++ tests_integration/lib/_definitions/__init__.py
@@ -1,4 +1,4 @@
-from . import conda_forge_pinning, fastapi, polars, pydantic
+from . import conda_forge_pinning, fastapi, polars, pydantic, zizmor
 from .base_classes import AbstractIntegrationTestHelper, GitHubAccount, TestCase
 
 TEST_CASE_MAPPING: dict[str, list[TestCase]] = {
@@ -6,6 +6,7 @@ TEST_CASE_MAPPING: dict[str, list[TestCase]] = {
     "fastapi": fastapi.ALL_TEST_CASES,
     "polars": polars.ALL_TEST_CASES,
     "pydantic": pydantic.ALL_TEST_CASES,
+    "zizmor": zizmor.ALL_TEST_CASES,
 }
 """
 Maps from feedstock name to a list of all test cases for that feedstock.
