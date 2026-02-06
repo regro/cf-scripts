850d63bfd77bf075fe44632b200665b1414b0248
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-02-06 13:05:44 -0600

    fix: set a timeout for git command execution in deploy
    Added a timeout for git commands to prevent hanging.

--- conda_forge_tick/deploy.py
+++ conda_forge_tick/deploy.py
@@ -27,7 +27,7 @@ from .utils import (
 logger = logging.getLogger(__name__)
 
 RNG = secrets.SystemRandom()
-
+GIT_CMD_TIMEOUT = 300
 
 def _flush_io():
     sys.stdout.flush()
@@ -35,7 +35,7 @@ def _flush_io():
 
 
 def _run_git_cmd(cmd, **kwargs):
-    r = subprocess.run(["git"] + cmd, check=True, **kwargs)
+    r = subprocess.run(["git"] + cmd, check=True, timeout=GIT_CMD_TIMEOUT, **kwargs)
     _flush_io()
     return r
 
@@ -86,6 +86,7 @@ def _pull_changes(batch):
         ["git", "pull", "-s", "recursive", "-X", "theirs"],
         text=True,
         capture_output=True,
+        timeout=GIT_CMD_TIMEOUT,
     )
     n_added = 0
     if r.returncode != 0:
@@ -204,6 +205,7 @@ def _get_files_to_delete() -> set[str]:
         text=True,
         capture_output=True,
         check=True,
+        timeout=GIT_CMD_TIMEOUT,
     )
     files_to_delete = set()
     for line in r.stdout.splitlines():
