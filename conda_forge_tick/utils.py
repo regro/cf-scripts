eb592d844a7aec71462a4f7b101eaa3a932be430
Min RK <benjaminrk@gmail.com>
2026-02-20 15:35:02 -0800

    ruff

--- conda_forge_tick/utils.py
+++ conda_forge_tick/utils.py
@@ -475,11 +475,19 @@ def replace_compiler_with_stub(text: str) -> str:
     # replaces ${{ compiler('c') }} with ${{ 'c_compiler_stub' }}
     # should work even with jinja filters like ${{ compiler('c') | replace('==', '>=') }}
     # where _only_ `compiler('c')` is replaced with the stub string
-    pattern = r'''(?<=\$\{\{)(.*)compiler\((["'])([^["']+)\2\)(?=.*\}\})'''
-    text = re.sub(pattern, lambda m: f"{m.group(1)}{m.group(2)}{m.group(3)}_compiler_stub{m.group(2)}", text)
+    pattern = r"""(?<=\$\{\{)(.*)compiler\((["'])([^["']+)\2\)(?=.*\}\})"""
+    text = re.sub(
+        pattern,
+        lambda m: f"{m.group(1)}{m.group(2)}{m.group(3)}_compiler_stub{m.group(2)}",
+        text,
+    )
 
-    pattern = r'''(?<=\$\{\{)(.*)stdlib\((["'])([^["']+)\2\)(?=.*\}\})'''
-    text = re.sub(pattern, lambda m: f"{m.group(1)}{m.group(2)}{m.group(3)}_stdlib_stub{m.group(2)}", text)
+    pattern = r"""(?<=\$\{\{)(.*)stdlib\((["'])([^["']+)\2\)(?=.*\}\})"""
+    text = re.sub(
+        pattern,
+        lambda m: f"{m.group(1)}{m.group(2)}{m.group(3)}_stdlib_stub{m.group(2)}",
+        text,
+    )
 
     return text
 
