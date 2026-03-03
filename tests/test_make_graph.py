f34e9df7aa227c6bfd3bd1393337784a67121ac2
beckermr <becker.mr@gmail.com>
2026-03-03 07:08:57 -0600

    refactor: remove duplicated code

--- tests/test_make_graph.py
+++ tests/test_make_graph.py
@@ -1,46 +1,11 @@
-import json
-import shutil
-import subprocess
-
 import pytest
-from conftest import FakeLazyJson
+from conftest import HAVE_CONTAINERS_AND_TEST_IMAGE, FakeLazyJson
 
 from conda_forge_tick.lazy_json_backends import LazyJson
 from conda_forge_tick.make_graph import try_load_feedstock
 
-HAVE_CONTAINERS = (
-    shutil.which("docker") is not None
-    and subprocess.run(["docker", "--version"], capture_output=True).returncode == 0
-)
 
-if HAVE_CONTAINERS:
-    HAVE_TEST_IMAGE = False
-    try:
-        for line in subprocess.run(
-            [
-                "docker",
-                "images",
-                "--format",
-                "json",
-            ],
-            check=True,
-            capture_output=True,
-            text=True,
-        ).stdout.splitlines():
-            image = json.loads(line)
-            if image["Repository"] == "conda-forge-tick" and image["Tag"] == "test":
-                HAVE_TEST_IMAGE = True
-                break
-    except subprocess.CalledProcessError as e:
-        print(
-            f"Could not list local docker images due "
-            f"to error {e}. Skipping container tests!"
-        )
-
-
-@pytest.mark.parametrize(
-    "container_enabled", [HAVE_CONTAINERS and HAVE_TEST_IMAGE, False]
-)
+@pytest.mark.parametrize("container_enabled", [HAVE_CONTAINERS_AND_TEST_IMAGE, False])
 @pytest.mark.parametrize("existing_archived", [True, False, None])
 @pytest.mark.parametrize("mark_not_archived", [True, False])
 def test_try_load_feedstock(
