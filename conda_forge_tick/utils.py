6097346574054a9033c989cf92f0b493108c77d3
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-03-21 13:02:38 -0500

    Apply suggestions from code review
    Co-authored-by: Matthew R. Becker <beckermr@users.noreply.github.com>

--- conda_forge_tick/utils.py
+++ conda_forge_tick/utils.py
@@ -510,16 +510,11 @@ def _render_recipe_yaml(
             with open(_cbc_path, "w") as fp:
                 fp.write(str(cbc_path))
 
-            variant_config_flags = ["-m", _cbc_path]
+            variant_config_flags = ["--variant-config", _cbc_path]
         else:
-            variant_config_flags = [] if cbc_path is None else ["-m", str(cbc_path)]
-
-        global_cbc_pth = os.path.join(
-            os.environ["CONDA_PREFIX"], "conda_build_config.yaml"
-        )
-        if os.path.exists(global_cbc_pth):
-            variant_config_flags = ["-m", global_cbc_pth] + variant_config_flags
-
+            variant_config_flags = (
+                [] if cbc_path is None else ["--variant-config", str(cbc_path)]
+            )
         target_platform_flags = (
             []
             if platform_arch is None or variant_config_flags
